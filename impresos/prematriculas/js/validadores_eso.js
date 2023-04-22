ce5b12867453fafb74f2bf3ca8cc0b765f017579
Pepito <dimicel@gmail.com>
2023-04-22 12:39:04 +0200

    .

--- impresos/prematriculas/js/validadores_eso.js
+++ impresos/prematriculas/js/validadores_eso.js
@@ -252,8 +252,7 @@ function creaValidatorPagina5_4eso() {
             else if($(element).attr('name') == "eso4_religion")
                 $(element).parent().parent().next().children().html(error);
             else if($(element).attr('name') == "eso4_bloque1")
-                //$(element).parent().parent().parent().prev().children().children().$(".errorTxt").html(error);
-                document.getElementById("errBloque1").innerHTML=error;
+                $(element).parent().parent().parent().prev().children().children().next().html(error);
             else $(element).parent().parent().next($('.errorTxt')).html(error);
         }
     });
