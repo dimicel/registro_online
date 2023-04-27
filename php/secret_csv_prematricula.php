f2dbcbdca53f707c30aa69e40853607bab7ffba7
Pepito <dimicel@gmail.com>
2023-04-27 16:38:48 +0200

    .

--- php/secret_csv_prematricula.php
+++ php/secret_csv_prematricula.php
@@ -33,29 +33,21 @@ elseif($tabla=="premat_3eso") $Datos='NIE;ALUMNO;SEXO;CURSO_ACTUAL;GRUPO;PROGRAM
 elseif($tabla=="premat_4eso") $Datos='NIE;ALUMNO;SEXO;CURSO_ACTUAL;GRUPO;PROGRAMA_LING;MATEMATICAS;PRIMER_IDIOMA;REL/VAL_ETICOS;OPC_BLOQUE1;OPC_BLOQUE2_1;OPC_BLOQUE2_2;OPC_BLOQUE2_3;OPC_BLOQUE2_4;OPC_BLOQUE3_1;OPC_BLOQUE3_2;OPC_BLOQUE3_3;OPC_BLOQUE3_4;OPC_BLOQUE3_5;OPC_BLOQUE3_6;OPTATIVA1;OPTATIVA2;OPTATIVA3;OPTATIVA4;OPTATIVA5'.PHP_EOL;
 elseif($tabla=="premat_3esodiv") $Datos='NIE;ALUMNO;SEXO;CURSO_ACTUAL;GRUPO;REL/AT.EDUC;OPTATIVA1;OPTATIVA2;OPTATIVA3'.PHP_EOL;
 elseif($tabla=="premat_4esodiv") $Datos='NIE;ALUMNO;SEXO;CURSO_ACTUAL;GRUPO;REL/AT.EDUC;OPCION1;OPCION12;OPCION13;OPCION14;OPCION15;OPCION16;OPTATIVA1;OPTATIVA2;OPTATIVA3;OPTATIVA4;OPTATIVA5'.PHP_EOL;
-elseif($tabla=="premat_1bach_h"){
-    $Datos='NIE;ALUMNO;SEXO;ITINERARIO;PRIMER_IDIOMA;TRONC_GEN1;TRONC_GEN2;TRONC_OPCION;';
-    $Datos.='ESPEC_ITIN1;ESPEC_ITIN2;ESPEC_ITIN3;ESPEC_ITIN4;ESPEC_ITIN5;ESPEC_ITIN6;ESPEC_ITIN7;ESPEC_ITIN8;';
-    $Datos.='ESPEC_COM1;ESPEC_COM2;ESPEC_COM3;ESPEC_COM4;ESPEC_COM5;ESPEC_COM6;ESPEC_COM7'.PHP_EOL;
-} 
-elseif($tabla=="premat_1bach_c"){
-    $Datos='NIE;ALUMNO;SEXO;ITINERARIO;PRIMER_IDIOMA;TRONC_GEN_OPC1;TRONC_GEN_OPC2;';
-    $Datos.='ESPEC_ITIN1;ESPEC_ITIN2;ESPEC_ITIN3;ESPEC_ITIN4;ESPEC_ITIN5;ESPEC_ITIN6;ESPEC_ITIN7;ESPEC_ITIN8;';
-    $Datos.='ESPEC_ITIN9;ESPEC_ITIN10;ESPEC_ITIN11;';
-    $Datos.='ESPEC_COM1;ESPEC_COM2;ESPEC_COM3;ESPEC_COM4;ESPEC_COM5;ESPEC_COM6;ESPEC_COM7'.PHP_EOL;
+elseif($tabla=="premat_1bach_h" || $tabla=="premat_1bach_c"){
+    $Datos='NIE;ALUMNO;SEXO;ITINERARIO;MODALIDAD;PRIMER_IDIOMA;REL/AT.EDUC;OBLIGATORIA1;OBLIGATORIA2;OBLIGATORIA3;';
+    $Datos.='OPTATIVA1;OPTATIVA2;OPTATIVA3;OPTATIVA4;OPTATIVA5;OPTATIVA6;OPTATIVA7;OPTATIVA8;';
+    $Datos.='OPTATIVA9;OPTATIVA10;OPTATIVA11;OPTATIVA12;OPTATIVA13;OPTATIVA14;OPTATIVA15'.PHP_EOL;
 } 
 elseif($tabla=="premat_2bach_h"){
-    $Datos='NIE;ALUMNO;SEXO;ITINERARIO;PRIMER_IDIOMA;TRONC_GEN;TRONC_OPC1;TRONC_OPC2;';
-    $Datos.='ESPEC_ITIM_COM1;ESPEC_ITIM_COM2;ESPEC_ITIM_COM3;ESPEC_ITIM_COM4;ESPEC_ITIM_COM5;ESPEC_ITIM_COM6;';
-    $Datos.='ESPEC_ITIM_COM7;ESPEC_ITIM_COM8;ESPEC_ITIM_COM9;ESPEC_ITIM_COM10;ESPEC_ITIM_COM11;ESPEC_ITIM_COM12;';
-    $Datos.='ESPEC_ITIM_COM13;ESPEC_ITIM_COM14;ESPEC_ITIM_COM15;ESPEC_ITIM_COM16;ESPEC_ITIM_COM17;'.PHP_EOL;
+    $Datos='NIE;ALUMNO;SEXO;PRIMER_IDIOMA;MODALIDAD1;MODALIDAD2;MODALIDAD3;';
+    $Datos.='OPTATIVA1;OPTATIVA2;OPTATIVA3;OPTATIVA4;OPTATIVA5;OPTATIVA6;OPTATIVA7;OPTATIVA8;';
+    $Datos.='OPTATIVA9;OPTATIVA10;OPTATIVA11;OPTATIVA12;OPTATIVA13;OPTATIVA14;OPTATIVA15;OPTATIVA16;OPTATIVA17'.PHP_EOL;
 } 
 elseif($tabla=="premat_2bach_c"){
-    $Datos='NIE;ALUMNO;SEXO;ITINERARIO;PRIMER_IDIOMA;TRONC_OPC1;TRONC_OPC2;';
-    $Datos.='ESPEC_ITIM_COM1;ESPEC_ITIM_COM2;ESPEC_ITIM_COM3;ESPEC_ITIM_COM4;ESPEC_ITIM_COM5;ESPEC_ITIM_COM6;';
-    $Datos.='ESPEC_ITIM_COM7;ESPEC_ITIM_COM8;ESPEC_ITIM_COM9;ESPEC_ITIM_COM10;ESPEC_ITIM_COM11;ESPEC_ITIM_COM12;';
-    $Datos.='ESPEC_ITIM_COM13;ESPEC_ITIM_COM14;ESPEC_ITIM_COM15;ESPEC_ITIM_COM16;ESPEC_ITIM_COM17;'.PHP_EOL;
-} 
+    $Datos='NIE;ALUMNO;SEXO;PRIMER_IDIOMA;MODALIDAD1;MODALIDAD2;MODALIDAD3;';
+    $Datos.='OPTATIVA1;OPTATIVA2;OPTATIVA3;OPTATIVA4;OPTATIVA5;OPTATIVA6;OPTATIVA7;OPTATIVA8;';
+    $Datos.='OPTATIVA9;OPTATIVA10;OPTATIVA11;OPTATIVA12;OPTATIVA13;OPTATIVA14;OPTATIVA15'.PHP_EOL;
+}
 
 header('Expires: 0');
 header('Cache-control: private');
@@ -80,13 +72,12 @@ while($r=$res->fetch_array(MYSQLI_ASSOC)){
         $Datos.=utf8_decode($r["curso_actual"].";");
         $Datos.=utf8_decode($r["grupo_curso_actual"].";");
         $Datos.=utf8_decode($r["prog_ling"].";");
-        $Datos.=utf8_decode($r["rel_valores_et"].";");
-        $Datos.=utf8_decode($r["1_lengua_extr"].";");
-        $Datos.=utf8_decode($r["optativa1"].";");
-        $Datos.=utf8_decode($r["optativa2"].";");
-        $Datos.=utf8_decode($r["optativa3"].";");
-        $Datos.=utf8_decode($r["optativa4"].";");
-        $Datos.=utf8_decode($r["optativa5"]).PHP_EOL;
+        $Datos.=utf8_decode($r["materia2"].";");
+        $Datos.=utf8_decode($r["materia1"].";");
+        $Datos.=utf8_decode($r["materia3"].";");
+        $Datos.=utf8_decode($r["materia4"].";");
+        $Datos.=utf8_decode($r["materia5"].";");
+        $Datos.=utf8_decode($r["optativa6"]).PHP_EOL;
     }
     elseif($tabla=="premat_3eso"){
         $Datos.=utf8_decode($r["id_nie"].";");
@@ -95,12 +86,12 @@ while($r=$res->fetch_array(MYSQLI_ASSOC)){
         $Datos.=utf8_decode($r["curso_actual"].";");
         $Datos.=utf8_decode($r["grupo_curso_actual"].";");
         $Datos.=utf8_decode($r["prog_ling"].";");
-        $Datos.=utf8_decode($r["rel_valores_et"].";");
-        $Datos.=utf8_decode($r["1_lengua_extr"].";");
-        $Datos.=utf8_decode($r["optativa1"].";");
-        $Datos.=utf8_decode($r["optativa2"].";");
-        $Datos.=utf8_decode($r["optativa3"].";");
-        $Datos.=utf8_decode($r["optativa4"]).PHP_EOL;
+        $Datos.=utf8_decode($r["materia2"].";");
+        $Datos.=utf8_decode($r["materia1"].";");
+        $Datos.=utf8_decode($r["materia3"].";");
+        $Datos.=utf8_decode($r["materia4"].";");
+        $Datos.=utf8_decode($r["materia5"].";");
+        $Datos.=utf8_decode($r["optativa6"]).PHP_EOL;
     }
     elseif($tabla=="premat_4eso"){
         $Datos.=utf8_decode($r["id_nie"].";");
