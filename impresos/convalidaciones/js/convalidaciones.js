4dbae8aa39435c6bd9b4bc9ffcf611186c965bf4
Pepito <dimicel@gmail.com>
2023-05-08 20:39:16 +0200

    .

--- impresos/convalidaciones/js/convalidaciones.js
+++ impresos/convalidaciones/js/convalidaciones.js
@@ -77,6 +77,7 @@ function vuelve(){
 }
 
 function selGrado(obj){
+    if (obj.value!="") obj.removeChild(obj.options[0]);
     $.post("php/listaciclos.php",{grado:obj.value},(resp)=>{
         if (resp["error"]=="servidor"){
             alerta("Hay un problema con el servidor. Inténtelo más tarde.","ERROR SERVIDOR");
