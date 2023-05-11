ed59d53b421b92d4fad3d0b9f07f4735eecc9534
Pepito <dimicel@gmail.com>
2023-05-11 19:20:24 +0200

    .

--- impresos/convalidaciones/js/convalidaciones.js
+++ impresos/convalidaciones/js/convalidaciones.js
@@ -145,7 +145,7 @@ function creaValidadorCentroMin(){
             }
         },
         errorPlacement: function(error, element) {
-            $("label[for='"+$(element).attr('name')+"']").next($('.errorTxt')).html(error);
+            //$("label[for='"+$(element).attr('name')+"']").next($('.errorTxt')).html(error);
         }
     });
 }
