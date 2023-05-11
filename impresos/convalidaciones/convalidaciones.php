06eaaa99b17f24d9ee3629914b7ceaab5e8a47d1
Pepito <dimicel@gmail.com>
2023-05-11 19:38:53 +0200

    .

--- impresos/convalidaciones/convalidaciones.php
+++ impresos/convalidaciones/convalidaciones.php
@@ -229,7 +229,7 @@
                                 </div>
                                 <div class="row">
                                     <div class="col" >
-                                        <table  style="width: 70%; margin: 0 auto;"><tr><td style="width:50%"><b>Descripción</b></td><td  style="width:50%"><b>Documento</b></td></tr></table>
+                                        <table  style="width: 70%; margin: 0 auto;background-color:lightslategrey"><tr><td style="width:50%"><b>Descripción</b></td><td  style="width:50%"><b>Documento</b></td></tr></table>
                                         <table id="tab_lista_docs"  style="width: 70%; margin: 0 auto;"><tr><td style="text-align:center">LISTA DE DOCUMENTOS VACÍA</td></tr></table>
                                     </div>
                                 </div>
