e74a9a0daaa12a9d237c244b48a056765e702491
Pepito <dimicel@gmail.com>
2023-05-13 19:07:13 +0200

    .

--- impresos/convalidaciones/php/registracentroministerio.php
+++ impresos/convalidaciones/php/registracentroministerio.php
@@ -99,19 +99,17 @@ try {
     
     // Insertar registros en la segunda tabla
     $stmt2 = $mysqli->prepare("INSERT INTO convalidaciones_docs (registro, descripcion, ruta) VALUES (?, ?, ?)");
-    
     for($i=0;$i<count($desc);$i++) {
         $indice=sprintf("%02d", $i+1)."_";
         $stmt2->bind_param("sss", $registro, $desc[$i], "docs/".$id_nie."/convalidaciones"."/".$anno_curso."/".$dirRegistro."/docs"."/".$indice.$docs[$i]["name"]);
         $stmt2->execute();
     }
-    
+    exit("aqui");
     $stmt2->close();
     $rutaCompleta=__DIR__."/../../../docs/".$id_nie."/"."convalidaciones/".$anno_curso."/".$dirRegistro."/docs"."/";
     if (!is_dir($rutaCompleta)) {
         mkdir($rutaCompleta, 0777, true);
     }
-    exit("aqui");
     if (isset($_FILES["docs"])){
         for ($i=0;$i<count($_FILES["docs"]["tmp_name"]);$i++){
             $indice=sprintf("%02d", $i+1)."_";
