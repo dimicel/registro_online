dbd1cdf01a23fadcc02f1da3a2da0ac5ff3e848f
Pepito <dimicel@gmail.com>
2023-05-13 20:28:52 +0200

    .

--- impresos/convalidaciones/php/registracentroministerio.php
+++ impresos/convalidaciones/php/registracentroministerio.php
@@ -273,7 +273,7 @@ $pdf->SetXY(35,189);
 $pdf->MultiCell(150,0,$modulos,0,'L',0,1,'','',true,0,false,false,0);
 
 $pdf->setCellHeightRatio(1.4);
-$pdf->SetXY(100,260);
+$pdf->SetXY(120,260);
 $pdf->Cell(0,0,$dia . " de " . $mes . " de " . $anno,0,0,'L',0,'',1,true,'T','T');
 
 
