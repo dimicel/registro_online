408e96e4348431149c19a76bb82dd110b967566b
Pepito <dimicel@gmail.com>
2023-05-13 20:36:03 +0200

    .

--- impresos/convalidaciones/php/registracentroministerio.php
+++ impresos/convalidaciones/php/registracentroministerio.php
@@ -273,9 +273,9 @@ $pdf->SetXY(35,189);
 $pdf->MultiCell(150,0,$modulos,0,'L',0,1,'','',true,0,false,false,0);
 
 $pdf->setCellHeightRatio(1.4);
-$pdf->SetXY(120,260);
+$pdf->SetXY(110,260);
 $pdf->Cell(0,0,$dia . " de " . $mes . " de " . $anno,0,0,'L',0,'',1,true,'T','T');
-$pdf->Image($_FILES["firma"]["tmp_name"], 120, 110, 0, 35);
+$pdf->Image($_FILES["firma"]["tmp_name"], 120, 240, 0, 35);
 //Poner el número de registro a la izquierda y vertical
 $pdf->StartTransform();
 $pdf->Rotate(90);
