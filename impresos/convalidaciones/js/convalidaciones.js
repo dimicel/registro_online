5c1abd4f7128d93a052a20dcb3677d7cc64b6ed9
Pepito <dimicel@gmail.com>
2023-05-14 00:35:10 +0200

    .

--- impresos/convalidaciones/js/convalidaciones.js
+++ impresos/convalidaciones/js/convalidaciones.js
@@ -516,7 +516,7 @@ function startDrawing(event) {
 
 
 function draw(event) {
-    if (drawing) {
+    /*if (drawing) {
       var currentX = event.pageX - canvas.offsetLeft;
       var currentY = event.pageY - canvas.offsetTop;
       
@@ -529,7 +529,28 @@ function draw(event) {
       // Actualizar las coordenadas del ratón
       mouseX = currentX;
       mouseY = currentY;
-    }
+    }*/
+    var x, y;
+
+	// Get the mouse position relative to the <canvas> element
+	if (ev.layerX || ev.layerX == 0) { // Firefox
+		x = ev.layerX;
+		y = ev.layerY;
+	} else if (ev.offsetX || ev.offsetX == 0) { // Opera
+		x = ev.offsetX;
+		y = ev.offsetY;
+	}
+
+	// The event handler works like a drawing pencil which
+	// tracks the mouse movements. We start drawing a path made up of lines
+	if (!started) {
+		context.beginPath();
+		context.moveTo(x, y);
+		started = true;
+	} else {
+		context.lineTo(x, y);
+		context.stroke();
+	}
   }
   
   function stopDrawing() {
