5b7ed3d0e386427d8b3a6380ca5d400731252161
Pepito <dimicel@gmail.com>
2023-05-13 21:20:49 +0200

    .

--- impresos/convalidaciones/php/registracentroministerio.php
+++ impresos/convalidaciones/php/registracentroministerio.php
@@ -274,13 +274,13 @@ $pdf->MultiCell(150,0,$modulos,0,'L',0,1,'','',true,0,false,false,0);
 
 
 /////Tratamiento de la imagen
-$pdf->Image($_FILES["firma"]["tmp_name"], 100, 235, 40, 0, '', '', '', false, 300);
+$pdf->Image($_FILES["firma"]["tmp_name"], 110, 235, 50, 0, '', '', '', false, 300);
 $pdf->SetFont('dejavusans', '', 9, '', true);
 $pdf->SetXY(101,227);
 $pdf->Cell(0,0,"FIRMA DEL SOLICITANTE y FECHA",0,0,'L',0,'',1,true,'T','T');
 
 $pdf->setCellHeightRatio(1.4);
-$pdf->SetXY(110,260);
+$pdf->SetXY(110,270);
 $pdf->Cell(0,0,$dia . " de " . $mes . " de " . $anno,0,0,'L',0,'',1,true,'T','T');
 
 
