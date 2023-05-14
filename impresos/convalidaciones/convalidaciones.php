c15d9b1c94a0cb77cb50a7af5d8d4172544d8d26
Pepito <dimicel@gmail.com>
2023-05-14 08:44:23 +0200

    .

--- impresos/convalidaciones/convalidaciones.php
+++ impresos/convalidaciones/convalidaciones.php
@@ -273,7 +273,7 @@
             </div>
         </form>
     </div>
-    <div id="div_canvas_firma" style="display:none;width:600px">
+    <div id="div_canvas_firma" style="display:none;">
         <label><small>Mentenga pulsado el botón del ratón para dibujar la firma</small></label><br>
         <div id="div_lienzo" style="width: 400px;height: 200px;border: 1px solid black;">
             
