7bb223bc07346679bd73f75c68c90e549b62122b
Pepito <dimicel@gmail.com>
2023-05-17 23:33:24 +0200

    .

--- impresos/convalidaciones/js/convalidaciones.js
+++ impresos/convalidaciones/js/convalidaciones.js
@@ -410,7 +410,7 @@ function creaInputs() {
     tipoFile.accept="application/pdf"
     tipoFile.addEventListener("change", function() {
         if (formulario=="Consejería" && document.querySelectorAll("#anade_documento_consejeria input[name=tipo_con]:checked".length==0)){
-            alerta("Debe seleccioanr antes un tipo de documento.","FALTA SELECCIÓN TIPO");
+            alerta("Debe seleccionar antes un tipo de documento.","FALTA SELECCIÓN TIPO");
         }
         if (this.multiple && this.files.length!=2){
             alerta("Debe seleccionar dos archivos de imagen: el anverso y reverso del documento de identificación.", "Nº INCORRECTO DE ARCHIVOS SELECCIONADOS")
