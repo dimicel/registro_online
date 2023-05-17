8d826621b7e2fba99e7db493b5194f3b167a51e7
Pepito <dimicel@gmail.com>
2023-05-17 07:59:22 +0200

    .

--- impresos/convalidaciones/convalidaciones.php
+++ impresos/convalidaciones/convalidaciones.php
@@ -318,7 +318,7 @@
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-2 custom-control custom-switch mi-checkbox">
+                    <div class="col-1 custom-control custom-switch mi-checkbox">
                         <input type="radio" id="otro" name="tipo_con" class="custom-control-input" value="Otro" />
                         <label for="otro" class="custom-control-label">Otro</label>
                     </div>
