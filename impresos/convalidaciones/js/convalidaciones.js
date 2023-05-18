09e8daae96a07a2a023317d8df1e0dbcf5067859
Pepito <dimicel@gmail.com>
2023-05-18 19:43:15 +0200

    .

--- impresos/convalidaciones/js/convalidaciones.js
+++ impresos/convalidaciones/js/convalidaciones.js
@@ -750,7 +750,7 @@ function muestraEditor(_ev){
         //document.getElementById("imagen_anverso").style.width="450px";
         //document.getElementById("imagen_anverso").style.height="255px";
         _crop1=new Croppie(document.getElementById("div_imagen_anverso"), {
-            viewport: { width: 300, height: 170 },
+            viewport: { width: 300, height: 190 },
             boundary: { width: 450, height: 255 },
             showZoomer: true,
             enableOrientation: true
