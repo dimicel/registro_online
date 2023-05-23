b233adb6ee932f19ab4bdc7ab8e56823f8746d5e
Pepito <dimicel@gmail.com>
2023-05-23 19:42:08 +0200

    .

--- impresos/convalidaciones/php/registraconsejeria.php
+++ impresos/convalidaciones/php/registraconsejeria.php
@@ -78,7 +78,8 @@ file_put_contents($tempFile, base64_decode(str_replace('data:image/png;base64,',
 $firma = $tempFile;
 
 if (isset($_FILES["pasaporte"])){
-    $imagePass = file_get_contents($_FILES['pasaporte']['tmp_name']);
+    //$imagePass = file_get_contents($_FILES['pasaporte']['tmp_name']);
+    $imagePass=$_POST['pasaporte'];
     $tempPass = tempnam(__DIR__."/../../../docs/tmp", 'pasaporte_'. session_id() . '.jpg');
     file_put_contents($tempPass, base64_decode(str_replace('data:image/jpg;base64,', '', $imagePass)));
     $pasaporte = $tempPass;
