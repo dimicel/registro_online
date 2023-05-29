51cf061041b4580c8fdb3263c022c6a0599be7cf
Pepito <dimicel@gmail.com>
2023-05-29 23:52:24 +0200

    .

--- js/usuario.js
+++ js/usuario.js
@@ -476,10 +476,10 @@ function listaSolicitudes() {
                     tabla += "<td style='width:90px;text-align:center;color:blue'>" + f_reg.substr(8, 2) + "-" + f_reg.substr(5, 2) + "-" + f_reg.substr(0, 4) + "</td>";
                     if (proc=="Convalidaciones"){
                         tabla += "<td style='color:blue'><a style='color:blue' href='docs/"+id_nie+"/"+item["dir"]+"/"+ item["curso"] + "/"+item["registro"].substring(17)+"/"+ item["registro"] + ".pdf' target='_blank'>" + item["registro"] + "</a>";
-                        if (item["procesado"]=="FAVORABLE"){
+                        if (item["resolucion"]=="FAVORABLE"){
                             tabla += "<a style='margin-left:10px' href=''docs/"+id_nie+"/"+item["dir"]+"/"+ item["curso"] + "/"+item["registro"].substring(17)+"/resolucion/resolucion.pdf' target='_blank' title='Ver resolución'>(Resolución FAVORABLE)</a>"
                         }
-                        else if(item["procesado"]=="NO FAVORABLE"){
+                        else if(item["resolucion"]=="NO FAVORABLE"){
                             tabla += "<a style='margin-left:10px' href=''docs/"+id_nie+"/"+item["dir"]+"/"+ item["curso"] + "/"+item["registro"].substring(17)+"/resolucion/resolucion.pdf' target='_blank' title='Ver resolución'>(Resolución NO FAVORABLE)</a>"
                         }
                         else{
