a79bd0c795516f299c91e50a0b68ba4e23930aa1
Pepito <dimicel@gmail.com>
2023-05-30 11:49:44 +0200

    .

--- php/secret_usu_expedienteusu.php
+++ php/secret_usu_expedienteusu.php
@@ -85,7 +85,7 @@ foreach($tipos_doc as $tipodoc=>$ruta){
 						if($tipodoc=="convalidaciones"){
 							if ($doc != "." && $doc != ".."){
 								$subConv=opendir("../docs/".$id_nie."/".$ruta."/".$dir."/".$doc);
-								$data["error"]=$subConv;
+								$data["error"]=$subConv . "  " . "../docs/".$id_nie."/".$ruta."/".$dir."/".$doc;
 										exit(json_encode($data));
 								while(false!=($docConv=readdir($subConv))){
 									if (is_file($docConv)){
