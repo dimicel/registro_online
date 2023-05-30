11da72dac575b539be92d95dffb9f161357359e1
Pepito <dimicel@gmail.com>
2023-05-30 11:59:43 +0200

    .

--- php/secret_usu_expedienteusu.php
+++ php/secret_usu_expedienteusu.php
@@ -87,8 +87,6 @@ foreach($tipos_doc as $tipodoc=>$ruta){
 								$subConv=opendir("../docs/".$id_nie."/".$ruta."/".$dir."/".$doc);
 								while(false!=($docConv=readdir($subConv))){
 									if ($docConv!="." && $docConv!=".." && $docConv!="docs"){
-										$data["error"]=$docConv;
-										exit(json_encode($data));
 										$data["error"]="ok";
 										$data["docs"][$tipodoc][$contador]["curso"]=$dir;
 										$data["docs"][$tipodoc][$contador]["doc"]=$docConv;
