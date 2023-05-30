fc06ff73d4cb307c1c6fa4922ca197e3079949aa
Pepito <dimicel@gmail.com>
2023-05-30 12:04:56 +0200

    .

--- js/secretaria_usu.js
+++ js/secretaria_usu.js
@@ -220,7 +220,6 @@ function verExpediente(id_nie, nom) {
 function obtieneDocsExpediente() {
     filtro_curso = document.getElementById("curso_exp").value;
     $.post("php/secret_usu_expedienteusu.php", { id_nie: document.getElementById("nie_exp").innerHTML, filtro: filtro_curso }, function(resp) {
-        alert(resp.error)
         if (resp.error == "server") alerta("Error en el servidor. Inténtalo más tarde.", "Error de servidor");
         else if (resp.error == "sin_registros") {
             contenido_div = "<center>NO EXISTEN DOCUMENTOS ASOCIADOS A ESTE NIE</center>";
@@ -232,6 +231,16 @@ function obtieneDocsExpediente() {
                 if (resp["docs"][td].length > 0) {
                     contenido_div += "<tr style='font-size:bolder'><td colspan=4 width='475px'>" + docs_exp[td] + "</td></tr>";
                     for (j = 0; j < resp["docs"][td].length; j++) {
+                        if(docs_exp[td] == "CONVALIDACIONES"){
+                            contenido_div += "<tr><td width='80px'>" + resp["docs"][td][j]["curso"] + "</td>";
+                            contenido_div += "<td><a href='" + resp["docs"][td][j]["enlace"] + "' target='_blank'>"
+                            contenido_div += resp["docs"][td][j]["doc"] + "</a><a onclick='adjuntosConvalid(\""+resp['docs'][td][j]['doc']+"\")>Adjuntos</a></td>";
+                        }
+                        else{
+                            contenido_div += "<tr><td width='80px'>" + resp["docs"][td][j]["curso"] + "</td>";
+                            contenido_div += "<td><a href='" + resp["docs"][td][j]["enlace"] + "' target='_blank'>"
+                            contenido_div += resp["docs"][td][j]["doc"] + "</a></td>";
+                        }
                         contenido_div += "<tr><td width='80px'>" + resp["docs"][td][j]["curso"] + "</td>";
                         contenido_div += "<td><a href='" + resp["docs"][td][j]["enlace"] + "' target='_blank'>"
                         contenido_div += resp["docs"][td][j]["doc"] + "</a></td>";
@@ -969,4 +978,9 @@ function bloqueaNomArch(){
     if (obj.value == "Documento de Identificación-Anverso" || obj.value == "Documento de Identificación-Reverso" || obj.value == "Fotografía del alumno" || obj.value == "Seguro Escolar") {
         document.getElementById("nom_doc").disabled=true;
     }
+}
+
+
+function adjuntosConvalid(registro){
+
 }
