a64313352d8d7cf62bb6d3daf26cfc2b3616c93e
Pepito <dimicel@gmail.com>
2023-05-30 10:35:44 +0200

    .

--- js/secretaria_usu.js
+++ js/secretaria_usu.js
@@ -422,6 +422,7 @@ function panelExpedienteUsuario(id_nie,nom) {
                 show: { effect: "fade", duration: 0 },
                 title: "EXPEDIENTE DEL USUARIO",
                 width: 500,
+                position: { my: "center top", at: "center top", of: window },
                 buttons: [{
                     class: "btn btn-success textoboton",
                     text: "Cerrar",
