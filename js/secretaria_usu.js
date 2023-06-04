ea3d10b331395bef8022794435daf3c4fa7a20a7
Pepito <dimicel@gmail.com>
2023-06-04 20:06:42 +0200

    .

--- js/secretaria_usu.js
+++ js/secretaria_usu.js
@@ -12,7 +12,7 @@ $(function() {
         callback: function(key, options) {
             id = $(this).children("td:first").html();
             nom = $(this).children("td:nth-child(2)").html();
-            alert(this.style.backgroundColor);
+            alert(this.innerHTML);
             if (key == "edit") {
                 panelModUsu(id);
             } else if (key == "delete") {
