e56210540d60f1739665fb1dcfbf5802bd167909
Pepito <dimicel@gmail.com>
2023-06-07 19:54:43 +0200

    .

---
+++ php/conexion.php
@@ -0,0 +1,31 @@
+<?php
+$dominio=$_SERVER["HTTP_HOST"];
+if ($dominio=="rotest.ulaboral.org"){
+	$servidor="localhost";
+	$usuario="ulaboral_myslq_dimi_fer";
+	$password="=WOyu;J6B&I6";
+	$db="ulaboral_imp_sec_online_test";
+}
+elseif ($dominio=="registro.ulaboral.org"){
+	$servidor="localhost";
+	$usuario="ulaboral_myslq_dimi_fer";
+	$password="=WOyu;J6B&I6";
+	$db="ulaboral_imp_sec_online";
+}
+else {
+	$servidor="localhost";
+	$usuario="root";
+	$password="#Ulab0ral@2223#";
+	$db="ulaboral_imp_sec_online";
+}
+
+if (!isset($mysqli)){		
+	$mysqli = new MySQLi($servidor, $usuarior, $password, $db);
+	if ($mysqli==false) {
+		return false;
+	}
+	else{
+		$mysqli->set_charset('utf8');
+		return true;
+	}
+}
