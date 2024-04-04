6ed37da4f26ec368b84074475b8b6ddee2609b1e
Pepito <dimicel@gmail.com>
2024-04-04 13:40:07 +0200

    .

--- php/limpia_reg.php
+++ php/limpia_reg.php
@@ -60,7 +60,7 @@ function borraCarpetas($directorioBase) {
     return rmdir($directorioBase);
 }
 $directorioBase = "docs/2";
-borraCarpetas($directorioBase);
+echo borraCarpetas($directorioBase);
 exit();
 $consulta="select * from usuarios where no_ha_entrado=1 order by id_nie";
 $result = $mysqli->query($consulta);
