ae6ea5a0640298ff084eb388b1ece52bb0639cb2
Pepito <dimicel@gmail.com>
2024-04-05 13:52:08 +0200

    .

--- secretaria.php
+++ secretaria.php
@@ -264,7 +264,7 @@
             </div>
             <div class="row justify-content-center" style="margin-top:10px">
                 <div class="col">
-                    <span style="margin-left:15px">Usuario con fondo rojo = Usuario INHABILITADO. Clic en botón derecho para habilitar/inhabilitar en el menú</span>
+                    <span style="text-align: center;">Usuario con fondo rojo = Usuario INHABILITADO. Clic en botón derecho para habilitar/inhabilitar en el menú</span>
                 </div>
             </div>
 
