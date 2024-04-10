48c0ae3e511ed109dd75aa809eb4a188667efa87
Pepito <dimicel@gmail.com>
2024-04-10 12:09:28 +0200

    .

--- residencia.php
+++ residencia.php
@@ -33,7 +33,7 @@
         <!--_______________________________________________________________________________________-->
         <div id="usu_reg_tab" class="ui-widget-header ui-corner-all" >
             <div class="row" style="margin-top:15px">
-                <div class="col-2" >
+                <div class="col-1" >
                     <label class="col-form-label" style="margin-left: 20px;">Año Acad.: </label>
                 </div>
                 <div class="col-2" >
