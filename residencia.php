cd704a0018b63bf7e3093fcef25f59003f61f1e8
Pepito <dimicel@gmail.com>
2024-04-10 12:02:31 +0200

    .

--- residencia.php
+++ residencia.php
@@ -31,7 +31,7 @@
        
         <!-- LISTADO USUARIOS _____________________________________________________________________-->
         <!--_______________________________________________________________________________________-->
-        <div id="usu_reg_tab" class="ui-widget-header ui-corner-all d-none" >
+        <div id="usu_reg_tab" class="ui-widget-header ui-corner-all" >
             <div class="row" style="margin-top:15px">
                 <div class="col-3" style="margin-left:-85px">
                     <label class="col-form-label col-lg-2" style="margin-left: 20px;">Año Acad.: </label>
