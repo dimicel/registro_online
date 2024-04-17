5372f42d91f2289a37d6e21cfc43ae8fc6bcf067
Pepito <dimicel@gmail.com>
2024-04-17 13:16:59 +0200

    .

--- php/secret_usu_borra_adjuntoconvalid.php
+++ php/secret_usu_borra_adjuntoconvalid.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 if (!isset($_SESSION['acceso_logueado']) || $_SESSION['acceso_logueado']!=="correcto") exit("Acceso denegado");
+include("conexion.php");
+header("Content-Type: text/html;charset=utf-8");
 if ($mysqli->errno>0) {
     exit("server");
 }
