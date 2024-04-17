5b5dbb84e0e213ddfd131a9efbf072868e42bf15
Pepito <dimicel@gmail.com>
2024-04-17 12:28:07 +0200

    .

--- js/secretaria_usu.js
+++ js/secretaria_usu.js
@@ -333,7 +333,6 @@ function borraDocExp(obj) {
 
 function confirmadoBorradoDoc() {
     doc_ruta = document.getElementById("del_ruta").value;
-    alert(doc_ruta);
     if (document.getElementById("doc_cod_seg").innerHTML == document.getElementById("t_doc_cod_seg").value) {
         $.post("php/secret_usu_borra_doc_exp.php", { ruta: doc_ruta }, function(resp) {
             document.getElementById("t_doc_cod_seg").value="";
