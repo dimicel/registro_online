27415d6b623008d73908913b101287a639fb4916
Pepito <dimicel@gmail.com>
2024-05-07 14:18:56 +0200

    .

--- php/secret_convalid_estado_resol.php
+++ php/secret_convalid_estado_resol.php
@@ -100,7 +100,6 @@ $dr=$concov->fetch_assoc();
 // Cerrar conexión
 $mysqli->close();
 
-exit("ok" . " " . $tipoPdfParaUsu);
 //Se genera el pdf para el alumno si están todos los módulos resueltos y, al menos, hay uno que resuelve el centro
 class MYPDF extends TCPDF {
 
