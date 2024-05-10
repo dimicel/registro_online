4f7002fb5b40465177f67a2157480d363c2491b0
Pepito <dimicel@gmail.com>
2024-05-10 12:35:02 +0200

    .

--- js/usuario.js
+++ js/usuario.js
@@ -490,14 +490,8 @@ function listaSolicitudes() {
                     tabla += "<td style='width:90px;text-align:center;color:blue'>" + f_reg.substr(8, 2) + "-" + f_reg.substr(5, 2) + "-" + f_reg.substr(0, 4) + "</td>";
                     if (proc=="Convalidaciones"){
                         tabla += "<td style='color:blue'><a style='color:blue' href='docs/"+id_nie+"/convalidaciones/"+ item["curso"] + "/"+item["registro"].slice(17)+"/"+ item["registro"] + ".pdf' target='_blank'>" + item["registro"] + "</a>";
-                        if (item["resolucion"]=="FAVORABLE"){
-                            tabla += "<a style='margin-left:10px;color:GREEN' href='docs/"+id_nie+"/convalidaciones/"+ item["curso"] + "/"+item["registro"].slice(17)+"/docs/resolucion/resolucion.pdf' target='_blank' title='Ver resolución'>(Resolución FAVORABLE)</a>"
-                        }
-                        else if(item["resolucion"]=="NO FAVORABLE"){
-                            tabla += "<a style='margin-left:10px;color:BROWN' href='docs/"+id_nie+"/convalidaciones/"+ item["curso"] + "/"+item["registro"].slice(17)+"/docs/resolucion/resolucion.pdf' target='_blank' title='Ver resolución'>(Resolución NO FAVORABLE)</a>"
-                        }
-                        else if(item["resolucion"]=="PARCIAL"){
-                            tabla += "<a style='margin-left:10px;color:ORANGE' href='docs/"+id_nie+"/convalidaciones/"+ item["curso"] + "/"+item["registro"].slice(17)+"/docs/resolucion/resolucion.pdf' target='_blank' title='Ver resolución'>(Resolución PARCIAL)</a>"
+                        if (item["resolucion"]=="PROCESADA"){
+                            tabla += "<a style='margin-left:10px;color:GREEN' href='docs/"+id_nie+"/convalidaciones/"+ item["curso"] + "/"+item["registro"].slice(17)+"/docs/resolucion/resolucion.pdf' target='_blank' title='Ver documento'>(Ver documento)</a>"
                         }
                         else{
                             tabla += " (Resolución EN ESPERA)"
