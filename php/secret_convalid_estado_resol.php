9e35860a4f6469c338101c46a88a47aa395c55e2
Pepito <dimicel@gmail.com>
2024-05-10 18:19:33 +0200

    .

--- php/secret_convalid_estado_resol.php
+++ php/secret_convalid_estado_resol.php
@@ -242,15 +242,13 @@ if($res_fav>0 || $res_nofav>0){
         $html.="</b><br>El centro educativo se pondrá en contacto con usted para darle instrucciones de cómo proceder.<br><br>";
     }
 
-    $html.="</b><br><br><br>";
-
     $meses = array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
     $fecha_actual=getdate();
     $dd=$fecha_actual["mday"];
     $mm=$meses[$fecha_actual["mon"]-1];
     $yyyy=$fecha_actual["year"];
     $fecha_firma="Toledo, a ".$dd." de ".$mm." de ".$yyyy;
-    $html.="<p style='text-align:\'center\''>".$fecha_firma."</p><br><br>";
+    $html.="<p style='text-align:center'>".$fecha_firma."</p><br><br>";
     $html.="<center><img src='../recursos/sello_firma.jpg' width='80' height='80'/></center><br><br>";
     $html.="<center>Fdo.: Luis Ángel Corrales Mariblanca</center>";
     $pdf->SetRightMargin($margen_derecho);
