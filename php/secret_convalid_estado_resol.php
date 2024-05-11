a5258fa248d0ff75ac54ba4e7a4f5bbe9ac4b774
Pepito <dimicel@gmail.com>
2024-05-11 08:29:02 +0200

    .

--- php/secret_convalid_estado_resol.php
+++ php/secret_convalid_estado_resol.php
@@ -294,17 +294,17 @@ elseif($res_min>0 || $res_con>0) {
             $image_file = '../recursos/logo_ccm.jpg';
             $this->Image($image_file, 10, 20, 25, '', 'JPG', '', 'T', false, 300, '', false, false, 0, false, false, false);
             $image_file = '../recursos/mini_escudo.jpg';
-		    $this->Image($image_file, 140, 20, 25, '', 'JPG', '', 'T', false, 300, '', false, false, 0, false, false, false);
+		    $this->Image($image_file, 140, 20, 20, '', 'JPG', '', 'T', false, 300, '', false, false, 0, false, false, false);
 
             $this->SetFont('helvetica', 'B', 12);
-            $this->SetXY(0,40);
+            $this->SetXY(0,50);
             $this->Cell(0,0,"INFORMACIÓN SOBRE LOS MÓDULOS QUE SOLICITA CONVALIDAR",0,0,'C',0,'',1,false,'T','T');
                 
             $this->SetFont('helvetica', '', 8);
             // Title
             //$this->setCellHeightRatio(1.75);
             $encab = "<label><strong>IES Universidad Laboral</strong><br>Avda. Europa, 28<br>45003-TOLEDO<br>Tlf.:925 22 34 00<br>Fax:925 22 24 54</label>";
-            $this->writeHTMLCell(0, 0, 160, 11, $encab, 0, 1, 0, true, 'C', true);
+            $this->writeHTMLCell(0, 0, 160, 20, $encab, 0, 1, 0, true, 'C', true);
             //$this->Ln();
             //$this->writeHTMLCell(0, 0, '', '', '', 'B', 1, 0, true, 'L', true);
         }
