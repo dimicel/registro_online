a8354aa235ad18c5391ce78e6dc76039d89f2f9e
Pepito <dimicel@gmail.com>
2024-05-14 21:15:48 +0200

    .

--- impresos/convalidaciones/php/listamodulos.php
+++ impresos/convalidaciones/php/listamodulos.php
@@ -15,8 +15,13 @@ $grado=$_POST["grado"];
 $curso=$_POST["curso"];
 
 
+if ($curso=="Virtual_Modular"){
+    $c=$mysqli->query("SELECT materias_ciclos.codigo,materias_ciclos.materia,materias_ciclos.curso FROM ciclos JOIN materias_ciclos ON ciclos.id=materias_ciclos.id WHERE ciclos.grado='$grado' AND ciclos.denominacion='$ciclo' ORDER BY materias_ciclos.materia");
+}
+else {
+    $c=$mysqli->query("SELECT materias_ciclos.codigo,materias_ciclos.materia,materias_ciclos.curso FROM ciclos JOIN materias_ciclos ON ciclos.id=materias_ciclos.id WHERE ciclos.grado='$grado' AND ciclos.denominacion='$ciclo' AND materias_ciclos.curso='$curso' ORDER BY materias_ciclos.materia");
+}
 
-$c=$mysqli->query("SELECT materias_ciclos.codigo,materias_ciclos.materia,materias_ciclos.curso FROM ciclos JOIN materias_ciclos ON ciclos.id=materias_ciclos.id WHERE ciclos.grado='$grado' AND ciclos.denominacion='$ciclo' AND materias_ciclos.curso='$curso' ORDER BY materias_ciclos.materia");
 if ($mysqli->errno>0){
     $resp["error"]="error_consulta ".$mysqli->errno;
     exit (json_encode($resp));
