151b833b4280ef018b1dcc8c00e2c79491ed491d
Pepito <dimicel@gmail.com>
2024-05-14 20:21:29 +0200

    .

--- js/secretaria.js
+++ js/secretaria.js
@@ -451,7 +451,7 @@ function listaRegistros(orden_campo, orden_direccion) {
     } else if(tipo_formulario=="convalidaciones"){
         tabla = tipo_formulario;
         campos = ["id_nie", "nombre", "fecha_registro","resuelve_cen","resuelto_cen","resuelve_con","resuelto_con","resuelve_min","resuelto_min"];
-        estilo = ["width:70px", "width:220px", "width:70px;text-align:center;", "width:70px;text-align:center;", "width:70px;text-align:center;", "width:70px;text-align:center;", "width:70px;text-align:center;", "width:70px;text-align:center;", "width:70px;text-align:center;", "width:70px;text-align:center;" ];
+        estilo = ["width:70px", "width:220px", "width:75px;text-align:center;", "width:70px;text-align:center;", "width:70px;text-align:center;", "width:70px;text-align:center;", "width:70px;text-align:center;", "width:70px;text-align:center;", "width:70px;text-align:center;", "width:70px;text-align:center;" ];
         encabezamiento = ["NIE", "Alumno", "Fecha Reg.","Centro","Proc.Centro","Consej.","Proc.Cons.","Minist.","Proc.Minist.","Visto"];
     } else if (tipo_formulario == "prematricula") {
         if (document.getElementById("curso_pre_mat").value == "2eso"){tabla = "premat_eso"; grupo="2º ESO";}
