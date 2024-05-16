511ec80121423ec6fd2a6848d555ca719c907992
Pepito <dimicel@gmail.com>
2024-05-16 12:12:19 +0200

    .

--- impresos/residencia/res.php
+++ impresos/residencia/res.php
@@ -84,11 +84,11 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
     <script src=<?php echo "../../jqueryui/jquery-ui.min.js?q=".time();?>></script>
+    <script src=<?php echo "../../js/jquery_validate/jquery.validate.min.js?q=".time();?>></script>
+    <script src=<?php echo "../../js/jquery_validate/additional-methods.min.js?q=".time();?>></script>
     <script src=<?php echo "../../js/comun.js?q=".time();?> type="text/javascript"></script>
     <script src=<?php echo "js/res.js?q=".time();?> type="text/javascript"></script>
     <script src=<?php echo "../../js/croppie/croppie.min.js?q=".time();?> type="text/javascript"></script>
-    <script src=<?php echo "../../js/jquery_validate/jquery.validate.min.js?q=".time();?>></script>
-    <script src=<?php echo "../../js/jquery_validate/additional-methods.min.js?q=".time();?>></script>
     <script src=<?php echo "js/validadores_res.js?q=".time();?> type="text/javascript"></script>
 
 </body>
