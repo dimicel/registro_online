abbbe4cee671d525d8a7552f645da13991f6fac9
Pepito <dimicel@gmail.com>
2024-05-22 11:50:51 +0200

    .

--- impresos/prematriculas/js/eso4_div.js
+++ impresos/prematriculas/js/eso4_div.js
@@ -8,10 +8,9 @@ function eso4div_generaDatosSerialize() {
     document.getElementById("eso4_opt3").value = document.getElementById("eso4div_opc").options[2].innerHTML;
     document.getElementById("eso4_opt4").value = document.getElementById("eso4div_opc").options[3].innerHTML;
     document.getElementById("eso4_opt5").value = document.getElementById("eso4div_opc").options[4].innerHTML;
-    document.getElementById("eso4_opt6").value = document.getElementById("eso4div_opc").options[5].innerHTML;
-    document.getElementById("eso4_opt7").value = document.getElementById("eso4div_optativas").options[0].innerHTML;
-    document.getElementById("eso4_opt8").value = document.getElementById("eso4div_optativas").options[1].innerHTML;
-    document.getElementById("eso4_opt9").value = document.getElementById("eso4div_optativas").options[2].innerHTML;
-    document.getElementById("eso4_opt10").value = document.getElementById("eso4div_optativas").options[3].innerHTML;
-    //document.getElementById("eso4_opt11").value = document.getElementById("eso4div_optativas").options[4].innerHTML;
+    document.getElementById("eso4_opt6").value = document.getElementById("eso4div_optativas").options[0].innerHTML;
+    document.getElementById("eso4_opt7").value = document.getElementById("eso4div_optativas").options[1].innerHTML;
+    document.getElementById("eso4_opt8").value = document.getElementById("eso4div_optativas").options[2].innerHTML;
+    document.getElementById("eso4_opt9").value = document.getElementById("eso4div_optativas").options[3].innerHTML;
+    document.getElementById("eso4_opt10").value = document.getElementById("eso4div_optativas").options[4].innerHTML;
 }
