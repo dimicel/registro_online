f0e8d3db781fe34892709452d3d20e5f7bbb9ee3
Pepito <dimicel@gmail.com>
2024-06-05 13:15:04 +0200

    .

--- prueba_sepa.php
+++ prueba_sepa.php
@@ -26,64 +26,64 @@ class MYPDF extends TCPDF {
 $pdf = new MYPDF('P', 'mm', 'A4', true, 'UTF-8', false);
 
 // set document information
-$pdf->SetCreator(PDF_CREATOR);
-$pdf->SetAuthor('IES Universidad Laboral');
-$pdf->SetTitle('Convalidaciones Centro Educativo y Ministerio');
-$pdf->SetSubject('Secretaría');
-$pdf->SetKeywords('ulaboral, PDF, secretaría, Toledo, Convalidaciones Centro Educativo y Ministerio');
+$pdf_sepa->SetCreator(PDF_CREATOR);
+$pdf_sepa->SetAuthor('IES Universidad Laboral');
+$pdf_sepa->SetTitle('Convalidaciones Centro Educativo y Ministerio');
+$pdf_sepa->SetSubject('Secretaría');
+$pdf_sepa->SetKeywords('ulaboral, PDF, secretaría, Toledo, Convalidaciones Centro Educativo y Ministerio');
 
 // set default monospaced font
-$pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+$pdf_sepa->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
 
 // set margins
-$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT, PDF_MARGIN_HEADER);
+$pdf_sepa->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT, PDF_MARGIN_HEADER);
 
 // set auto page breaks
-$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+$pdf_sepa->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
 
 // set image scale factor
-$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+$pdf_sepa->setImageScale(PDF_IMAGE_SCALE_RATIO);
 
 // ---------------------------------------------------------
 
-$pdf->setFontSubsetting(true);
+$pdf_sepa->setFontSubsetting(true);
 
-$pdf->SetFont('dejavusans', '', 8, '', true);
-$pdf->setFillColor(200);  //Relleno en gris
+$pdf_sepa->SetFont('dejavusans', '', 8, '', true);
+$pdf_sepa->setFillColor(200);  //Relleno en gris
 //Padding dentro de la celda del texto
-$pdf->setCellPaddings(0,0,0,0);
+$pdf_sepa->setCellPaddings(0,0,0,0);
 //Interlineado
-$pdf->setCellHeightRatio(1.5);
+$pdf_sepa->setCellHeightRatio(1.5);
 
 
-$pdf->AddPage();
+$pdf_sepa->AddPage();
 // get the current page break margin
-$bMargin = $pdf->getBreakMargin();
+$bMargin = $pdf_sepa->getBreakMargin();
 // get current auto-page-break mode
-$auto_page_break = $pdf->getAutoPageBreak();
+$auto_page_break = $pdf_sepa->getAutoPageBreak();
 // disable auto-page-break
-$pdf->SetAutoPageBreak(false, 0);
+$pdf_sepa->SetAutoPageBreak(false, 0);
 // set background image
-$pdf->Image("sepa.jpg", 0, 0, 210, 297, '', '', '', false, 300, '', false, false, 0);
+$pdf_sepa->Image("sepa.jpg", 0, 0, 210, 297, '', '', '', false, 300, '', false, false, 0);
 // restore auto-page-break status
-$pdf->SetAutoPageBreak($auto_page_break, $bMargin);
+$pdf_sepa->SetAutoPageBreak($auto_page_break, $bMargin);
 // set the starting point for the page content
-$pdf->setPageMark();
+$pdf_sepa->setPageMark();
 
-$pdf->SetXY(22,129);
-$pdf->Cell(0,0,$nombre . " " . $apellidos,0,0,'L',0,'',1,true,'T','T');
-$pdf->SetXY(22,139);
-$pdf->Cell(0,0,$direccion,0,0,'L',0,'',1,true,'T','T');
-$pdf->SetXY(22,148);
-$pdf->Cell(0,0,$cp." -" .$localidad." - ".$provincia,0,0,'L',0,'',1,true,'T','T');
+$pdf_sepa->SetXY(22,129);
+$pdf_sepa->Cell(0,0,$nombre . " " . $apellidos,0,0,'L',0,'',1,true,'T','T');
+$pdf_sepa->SetXY(22,139);
+$pdf_sepa->Cell(0,0,$direccion,0,0,'L',0,'',1,true,'T','T');
+$pdf_sepa->SetXY(22,148);
+$pdf_sepa->Cell(0,0,$cp." -" .$localidad." - ".$provincia,0,0,'L',0,'',1,true,'T','T');
 
 for ($i=0;$i<strlen($bic);$i++){
-    $pdf->SetXY(22+$i*7.5,169);
-    $pdf->Cell(0,0,substr($bic,$i,1),0,0,'L',0,'',1,true,'T','T');
+    $pdf_sepa->SetXY(22+$i*7.5,169);
+    $pdf_sepa->Cell(0,0,substr($bic,$i,1),0,0,'L',0,'',1,true,'T','T');
 }
 for ($i=0;$i<strlen($iban);$i++){
-    $pdf->SetXY(22+$i*6,184);
-    $pdf->Cell(0,0,substr($iban,$i,1),0,0,'L',0,'',1,true,'T','T');
+    $pdf_sepa->SetXY(22+$i*6,184);
+    $pdf_sepa->Cell(0,0,substr($iban,$i,1),0,0,'L',0,'',1,true,'T','T');
 }
 
 
@@ -102,14 +102,14 @@ $formateador = new IntlDateFormatter(
 // Formatear la fecha
 $fechaFormateada = $formateador->format($fecha);
 
-$pdf->SetXY(25,207);
-$pdf->Cell(0,0,$localidad." , a " . $fechaFormateada,0,0,'L',0,'',1,true,'T','T');
+$pdf_sepa->SetXY(25,207);
+$pdf_sepa->Cell(0,0,$localidad." , a " . $fechaFormateada,0,0,'L',0,'',1,true,'T','T');
 
-$pdf->Image("recursos/logo_ccm.jpg", 90, 210, 35, '', 'JPG', '', 'T', false, 300, '', false, false, 0, false, false, false);
+$pdf_sepa->Image("recursos/logo_ccm.jpg", 90, 210, 35, '', 'JPG', '', 'T', false, 300, '', false, false, 0, false, false, false);
 
 header("Content-Type: application/pdf");
 header("Content-Disposition: attachment; filename=" . $nombre_fichero);
-$pdf->Output($nombre_fichero, 'I');
+$pdf_sepa->Output($nombre_fichero, 'I');
 
 
 
