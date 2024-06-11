77296159992fefd65f35ea74224b89d963e41dea
Pepito <dimicel@gmail.com>
2024-06-11 11:45:58 +0200

    .

--- impresos/residencia/php/generapdf.php
+++ impresos/residencia/php/generapdf.php
@@ -689,8 +689,7 @@ if($_POST["nombre_foto"]!=""){
 //GENERA EL ARCHIVO NUEVO
 $nombre_fichero=recortarSustituirYObtener4Caracteres($apellidos).", ".recortarSustituirYObtener4Caracteres($nombre).".pdf";
 
-$respuesta["status"]=$email_jef_res;
-exit(json_encode($respuesta));
+
 if (strlen($email_jef_res)>0){
 	$adjunto=$pdf_salud->Output('', 'S');
 	$mail->addAddress($email_jef_res, 'Jefe Residencia');//jjgp46@educastillalamancha.es
