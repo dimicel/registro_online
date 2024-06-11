99c1dac01fb59a7126f31a81f2692ce4f9843eb7
Pepito <dimicel@gmail.com>
2024-06-11 11:16:19 +0200

    .

--- impresos/residencia/php/generapdf.php
+++ impresos/residencia/php/generapdf.php
@@ -259,12 +259,13 @@ class MYPDF extends TCPDF {
 	public function Header() {
 		
 		global $nombre_centro_edu, $direccion_centro_edu, $cp_centro_edu, $localidad_centro_edu, $tlf_centro_edu, $fax_centro_edu;
-		$respuesta["status"]="prueba";
-		exit(json_encode($respuesta));
+		
 		// Logo
 		//$image_file = '../recursos/logo_ccm.jpg';
 		//$this->Image($image_file, 10, 10, 25, '', 'JPG', '', 'T', false, 300, '', false, false, 0, false, false, false);
 		$image_file = '../recursos/mini_escudo.jpg';
+		$respuesta["status"]="prueba";
+		exit(json_encode($respuesta));
 		$this->Image($image_file, 140, 10, 20, '', 'JPG', '', 'T', false, 300, '', false, false, 0, false, false, false);
 
 		$this->SetFont('helvetica', 'B', 14);
