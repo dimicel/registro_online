c3d74068aaf1d0f59ecdcdade5955c1f0fe4d8cc
Pepito <dimicel@gmail.com>
2024-06-11 22:46:04 +0200

    .

--- js/secretaria.js
+++ js/secretaria.js
@@ -1905,7 +1905,7 @@ function tipoDocAdjuntoConvalid(obj){
 function adjuntaDocAdicional(_id_nie,registro){
     //Para convalidaciones
     c="<div class='row'>";
-    c+="<div class='col-2'><label for='tipo_doc_conval' class='col-form-label'>Clase Doc.: </label></div>";
+    c+="<div class='col-1'><label for='tipo_doc_conval' class='col-form-label'>Clase Doc.: </label></div>";
     c+="<div class='col-3'><select class='form-control' id='tipo_doc_conval' name='tipo_doc_conval' size='1' onchange='tipoDocAdjuntoConvalid(this);'/>";
     c+="<option value=''>Selecciona uno...</option>";
     c+="<option value='Resolución del Ministerio'>Resolución del Ministerio</option>";
@@ -1915,7 +1915,7 @@ function adjuntaDocAdicional(_id_nie,registro){
     c+="<div class='col-1'><label for='desc_adic_conval' class='col-form-label'>Descripción: </label></div>";
     c+="<div class='col-3'><input type='text' class='form-control' id='desc_adic_conval' name='desc_adic_conval' maxlength='40' readonly/></div>";
     c+="<div class='col-1'><label for='doc_adic_conval' class='col-form-label'>Documento: </label></div>";
-    c+="<div class='col'><input type='text' class='form-control' id='doc_adic_conval' readonly placeholder='Seleccionar documento' onclick='document.getElementById(\"conval_doc_adicional\").click()'/></div>";
+    c+="<div class='col-3'><input type='text' class='form-control' id='doc_adic_conval' readonly placeholder='Seleccionar documento' onclick='document.getElementById(\"conval_doc_adicional\").click()'/></div>";
     c+="</div>";
     c+="<input type='file' id='conval_doc_adicional' name='conval_doc_adicional' multiple='false' accept='application/pdf' style='position:absolute;left:-9999px' onchange='document.getElementById(\"doc_adic_conval\").value=this.files[0].name'/>";
     document.getElementById("div_dialogs2").innerHTML=c;
