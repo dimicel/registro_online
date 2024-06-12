c2af64dc0ea5098376bbf7f98e79f1b3a88c3499
Pepito <dimicel@gmail.com>
2024-06-12 12:18:52 +0200

    .

--- php/secret_actualiza_param_centro.php
+++ php/secret_actualiza_param_centro.php
@@ -9,12 +9,28 @@ if ($mysqli->errno>0) {
     exit("server");
 }
 
+$director=$_POST['director'];
+$centro=$_POST['centro'];
+$direccion=$_POST['direccion'];
+$cp=$_POST['cp'];
+$localidad=$_POST['localidad'];
+$provincia=$_POST['provincia'];
+$tlf_centro=$_POST['tlf_centro'];
+$fax_centro=$_POST['fax_centro'];
+$email_centro=$_POST['email_centro'];
+$email_jef_res=$_POST['email_jef_res'];
+$fianza_bonif=$_POST['finza_bonif'];
+$fianza_nobonif=$_POST['finza_nobonif'];
 
+
+$sql="update config_centro set director='$director',email_jefe_residencia='$email_jef_res',centro='$centro',cp_centro='$cp',direccion_centro='$direccion',";
+$sql.="localidad_centro='$localidad',provincia_centro='$provincia',tlf_centro='$tlf_centro',fax_centro='$fax_centro',email_centro='$email_centro',";
+$sql.="residencia_fianza_bonificados='$fianza_bonif',residencia_fianza_no_bonificados='$fianza_nobonif' where 1";
 if ($conn->query($sql) === TRUE) {
     if ($conn->affected_rows > 0) {
         exit("ok");
     } else {
-        exit("DATABASE");
+        exit("database");
     }
 } else {
     exit("Error al actualizar el registro: " . $conn->error);
