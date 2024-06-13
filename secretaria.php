80ba9cc8172df4955428a6d0ac9033b573aa85b4
Pepito <dimicel@gmail.com>
2024-06-13 18:13:01 +0200

    .

--- secretaria.php
+++ secretaria.php
@@ -493,13 +493,16 @@
             </div>
             <div class="row" style="height:300px;">
                 <div class="col-4" style="display: flex;justify-content: center;align-items: center;height: 100%;">
-                    <a href="#" ondblclick="subeLogo('logo_centro')"><img src="recursos/escudo.jpg" width="240"></a>
+                    <a href="#" ondblclick="document.getElementById('logo_centro').click()"><img src="recursos/escudo.jpg" width="240"></a>
+                    <input type="file" id="logo_centro" name="logo_centro" style="position: absolute; left:-9999px" accept="image/jpeg" onchange="subeLogo('logo_centro')" />
                 </div>
                 <div class="col-4" style="display: flex;justify-content: center;align-items: center;height: 100%;">
-                    <a href="#" ondblclick="subeLogo('logo_junta')"><img src="recursos/logo_ccm.jpg" width="240"></a>
+                    <a href="#" ondblclick="document.getElementById('logo_junta').click()"><img src="recursos/logo_ccm.jpg" width="240"></a>
+                    <input type="file" id="logo_junta" name="logo_junta" style="position: absolute; left:-9999px" accept="image/jpeg" onchange="subeLogo('logo_centro')" />
                 </div>
                 <div class="col-4" style="display: flex;justify-content: center;align-items: center;height: 100%;">
-                    <a href="#" ondblclick="subeLogo('firma_sello')"><img src="recursos/sello_firma.jpg" width="240"></a>
+                    <a href="#" ondblclick="document.getElementById('firma_sello').click()"><img src="recursos/sello_firma.jpg" width="240"></a>
+                    <input type="file" id="firma_sello" name="firma_sello" style="position: absolute; left:-9999px" accept="image/jpeg" onchange="subeLogo('firma_sello')" />
                 </div>
             </div>
             <div class="row">
