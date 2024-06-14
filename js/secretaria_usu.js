c2c453262bba8059a9062c023bcf4c1576ea0654
Pepito <dimicel@gmail.com>
2024-06-14 19:02:56 +0200

    .

--- js/secretaria_usu.js
+++ js/secretaria_usu.js
@@ -1104,7 +1104,7 @@ function confirmadoBorradoAdjuntoConvalid() {
             } else if (resp == "ok") {
                 alerta("Documento borrado con éxito.", "BORRADO OK");
                 if (refresca_docs=='0')regeneraListaAdjuntosConvalid();
-                else verRegAdjuntosConvalid(registro);
+                else {alert(0);verRegAdjuntosConvalid(registro);}
             }
             else if (resp == "server") {
                 alerta("Documento adjunto no eliminado, porque no se ha podido eliminar el registro asociado en la base de datos", "ERROR DB");
