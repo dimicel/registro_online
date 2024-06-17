f157b80b1b88bfa5ef6c15ffe07200c50617254c
Pepito <dimicel@gmail.com>
2024-06-17 09:42:28 +0200

    .

--- impresos/orden_sepa/php/datos_residente.php
+++ impresos/orden_sepa/php/datos_residente.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if (!isset($_SESSION['acceso_logueado']) || $_SESSION['acceso_logueado']!=="correcto") exit("Acceso denegado");
-include("conexion.php");
+include("../../../php/conexion.php");
 header("Content-Type: text/html;charset=utf-8");
 
 if ($mysqli->errno>0) {
