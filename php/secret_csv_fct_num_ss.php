1b478c4f76e13b1739a04bf1e9974676fab74fc2
dimicel <39248083+dimicel@users.noreply.github.com>
2024-09-12 01:06:13 +0200

    .

---
+++ php/secret_csv_fct_num_ss.php
@@ -0,0 +1,53 @@
+<?php
+session_start();
+if (!isset($_SESSION['acceso_logueado']) || $_SESSION['acceso_logueado']!=="correcto") exit("Acceso denegado");
+$error="";
+$Datos="";
+
+include("conexion.php");
+if ($mysqli->errno>0) $error="Error en servidor.";
+
+$curso=$_POST["curso_csv_seguro"];
+
+$anno_calculo=substr($curso,0,4);
+
+$res=$mysqli->query("SELECT u.apellidos, u.nombre, ud.num_ss 
+        FROM usuarios u 
+        JOIN usuarios_dat ud 
+        ON u.id_nie = ud.id_nie
+        WHERE ud.num_ss IS NOT NULL AND ud.num_ss <> ''
+        ORDER BY u.apellidos ASC, u.nombre ASC");
+
+if ($res->num_rows==0){
+    $error="No hay registros que listar.";
+}
+
+$Name = 'listado_num_ss.csv';
+$FileName = "./$Name";
+
+$Datos='NIE;ALUMNO;Nº SEGURIDAD SOCIAL'.PHP_EOL;
+
+header('Expires: 0');
+header('Cache-control: private');
+header('Content-Type: application/x-octet-stream;charset=utf-8'); // Archivo de Excel
+header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+header('Content-Description: File Transfer');
+header('Last-Modified: '.date('D, d M Y H:i:s'));
+header('Content-Disposition: attachment; filename="'.$Name.'"');
+header("Content-Transfer-Encoding: binary");
+
+if ($error!="") {
+    echo $error;
+    exit();
+}
+while($r=$res->fetch_array(MYSQLI_ASSOC)){
+    if(substr(strtoupper($r["id_nie"]),0,1)== "P") continue;//Los NIE que empiezan por P son usuarios de prueba
+
+    $Datos.="'".utf8_decode($r["id_nie"])."'".";";
+    $Datos.=utf8_decode(ucwords(strtolower($r["apellidos"])).", ".ucwords(strtolower($r["nombre"])).";");
+    $Datos.=utf8_decode($r["num_ss"].";");
+    $Datos.=utf8_decode($paga_seguro).PHP_EOL;		
+}
+
+echo $Datos;
+
