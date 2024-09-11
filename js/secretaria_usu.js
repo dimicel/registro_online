b2c23f363166416a61a30192501ca8b9c8a8eb65
dimicel <39248083+dimicel@users.noreply.github.com>
2024-09-11 12:03:46 +0200

    .

--- js/secretaria_usu.js
+++ js/secretaria_usu.js
@@ -645,7 +645,7 @@ function panelModUsu(id) {
                     if (f_nac != "") f_nac = f_nac.substr(8, 2) + "/" + f_nac.substr(5, 2) + "/" + f_nac.substr(0, 4);
                     form_modif_datos_usu.dat_sexo.value = resp.datos.sexo;
                     form_modif_datos_usu.dat_fecha_nac.value = f_nac;
-                    form_modif_datos_usu.dat_telefono.value = resp.datos.telef_alumno;U
+                    form_modif_datos_usu.dat_telefono.value = resp.datos.telef_alumno;
                     form_modif_datos_usu.dat_nss.value = resp.datos.num_ss;
                     form_modif_datos_usu.dat_email.value = resp.datos.email;
                     form_modif_datos_usu.dat_direccion.value = resp.datos.direccion;
