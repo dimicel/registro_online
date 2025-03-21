4d50a08b66be7971e535ff2f817cf57b20ab8025
Pepe Pepito <dimicel@gmail.com>
2025-03-21 12:47:23 +0100

    .

--- js/secretaria_usu.js
+++ js/secretaria_usu.js
@@ -276,7 +276,7 @@ function obtieneDocsExpediente() {
                             contenido_div += "<tr><td width='80px'>" + resp["docs"][td][j]["curso"] + "</td>";
                             contenido_div += "<td><a href='" + resp["docs"][td][j]["enlace"] + "' target='_blank'>"
                             contenido_div += resp["docs"][td][j]["doc"] + "</a><a style='margin-left:5px' onclick='adjuntosConvalid(\""+resp['docs'][td][j]['doc']+"\")'>(Adjuntos)</a>";
-                            if (resp["docs"][td][tj]["resolucion"]==""){
+                            if (resp["docs"][td][j]["resolucion"]==""){
                                 contenido_div +="</td>";
                             }
                             else{
