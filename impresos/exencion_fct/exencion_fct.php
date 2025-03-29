edc0a54c329e4f71fd1dee8e62b33a120adc30d7
dimicel <39248083+dimicel@users.noreply.github.com>
2025-03-29 16:03:47 +0100

    .

--- impresos/exencion_fct/exencion_fct.php
+++ impresos/exencion_fct/exencion_fct.php
@@ -35,7 +35,7 @@
             </div>
         </div>
     </div>
-    <div class="container" style="width:35%">    
+    <div class="container" style="width:38%">    
         <label style="color:red !important; margin-top:30px">* Campos obligatorios</label>
 
         <!-- FORMULARIO --------------------------------------------------------------------------------------->
