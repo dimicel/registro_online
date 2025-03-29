12eb10b41be3fa5ef2696770aa5281f6c00c564a
dimicel <39248083+dimicel@users.noreply.github.com>
2025-03-29 14:11:26 +0100

    .

--- impresos/exencion_fct/exencion_fct.php
+++ impresos/exencion_fct/exencion_fct.php
@@ -17,7 +17,7 @@
 </head>
 
 <body>
-    <div class="container w-75">
+    <div class="container w-50">
         <!--CABECERA LOGOS ------------------------------------------------------------------------------------>
         <div class="d-flex flex-row justify-content-center" style="margin-top:30px">
             <div class="col-2 justify-content-start">
