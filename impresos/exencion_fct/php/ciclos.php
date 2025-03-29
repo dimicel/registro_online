3ee1368a79dbf4fb6fbad621760506e74767070c
dimicel <39248083+dimicel@users.noreply.github.com>
2025-03-29 21:25:37 +0100

    .

---
+++ impresos/exencion_fct/php/ciclos.php
@@ -0,0 +1,33 @@
+<?php
+
+session_start();
+if (!isset($_SESSION['acceso_logueado']) || $_SESSION['acceso_logueado']!=="correcto") exit("Acceso denegado");
+include("../../../php/conexion.php");
+
+
+if ($mysqli->errno>0) {
+    $resp["error"]="servidor";
+    exit (json_encode($resp));
+}
+
+
+$c=$mysqli->query("select * from ciclos order by grado,denominacion");
+if ($mysqli->errno>0){
+    $resp["error"]="error_consulta";
+    exit (json_encode($resp));
+}
+elseif ($c->num_rows==0){
+    $resp["error"]="no_ciclos";
+    exit (json_encode($resp));
+}
+else {
+    $cont=0;
+    while($r=$c->fetch_assoc()){
+        $resp["datos"][$cont]["grado"]=$r["grado"];
+        $resp["datos"][$cont]["denominacion"]=$r["denominacion"];
+        $resp["datos"][$cont]["departamento"]=$r["departamento"];
+        $cont++;
+    }
+    $resp["error"]="ok";
+    exit (json_encode($resp));
+}
