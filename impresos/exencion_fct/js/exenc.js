bb1325e0210b75d8cc946c97a0613ed1a913b180
dimicel <39248083+dimicel@users.noreply.github.com>
2025-03-29 22:41:18 +0100

    .

--- impresos/exencion_fct/js/exenc.js
+++ impresos/exencion_fct/js/exenc.js
@@ -31,6 +31,7 @@ $(document).ready(function() {
     dat2 = dat1.then(() => {
         $.post("php/ciclos.php", { }, (resp) => {
             if (resp.error == "ok") {
+                alert(resp.datos.length)
                 for (i=0; i<resp.datos.length; i++){
                     let cont_bas=0;
                     let cont_med=0;
