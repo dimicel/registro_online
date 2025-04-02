f40afc8a57a26d2fdae7c2519e3be2ad213c55c3
dimicel <39248083+dimicel@users.noreply.github.com>
2025-04-02 12:19:06 +0200

    .

--- impresos/exencion_fct/exencion_fct.php
+++ impresos/exencion_fct/exencion_fct.php
@@ -101,7 +101,6 @@
                     <div class="row">
                         <div class="col" >
                             <span class="errorTxt" style="font-size: 1em;"></span>
-                            <input type="hidden" name="validar_tabla">
                             <table  style="width: 90%; margin: 0 auto;background-color:lightslategrey"><tr><td style="width:50%"><b>Descripción</b></td><td  style="width:50%"><b>Documento</b></td></tr></table>
                             <table id="tab_lista_docs"  style="width: 90%; margin: 0 auto;"><tr><td style="text-align:center">LISTA DE DOCUMENTOS VACÍA</td></tr></table>
                         </div>
