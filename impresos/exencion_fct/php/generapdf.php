547109be9ed0ecf2a8582d2f20cf27ebc521613c
dimicel <39248083+dimicel@users.noreply.github.com>
2025-04-07 12:52:10 +0200

    .

--- impresos/exencion_fct/php/generapdf.php
+++ impresos/exencion_fct/php/generapdf.php
@@ -60,7 +60,7 @@ function quitaAcentos($s){
 }
 
 
-$anno_curso= $_POST['curso'];
+$anno_curso= $_POST['anno_curso'];
 $fecha_registro=date('Y-m-d');
 $id_nie=$_POST['id_nie'];
 $lista_don=$_POST['lista_don'];
