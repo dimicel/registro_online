408e72c2eb5a356e71980bdb25a310189a31e7ed
dimicel <39248083+dimicel@users.noreply.github.com>
2025-04-08 10:30:30 +0200

    .

--- secretaria.php
+++ secretaria.php
@@ -236,7 +236,7 @@
                     </div>
                     <div class="row" style="display:none" id="div_exencion_fct">
                         <label class="col-form-label col-1" style="margin-left:20px">Departamento: </label>
-                        <div class="col-6" style="margin-left:-35px">
+                        <div class="col-6" >
                             <select id="departamento" class="form-control" onchange="cambioDepartamento(this.value)"></select>
                         </div>
                         <div class="col" style="margin-left:-35px">
