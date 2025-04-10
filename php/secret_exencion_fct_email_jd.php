34d78b6fd3086f9cc945a1c00c64a41bf0ea25f5
dimicel <39248083+dimicel@users.noreply.github.com>
2025-04-10 15:43:58 +0200

    .

--- php/secret_exencion_fct_email_jd.php
+++ php/secret_exencion_fct_email_jd.php
@@ -46,7 +46,7 @@ if ($result->num_rows > 0) {
 $error="<ul>";
 $mysqli->close();
 $error_generado=false;
-if(filter_var(trim(preg_replace('/[^\x20-\x7E]/', '', $envios_email[$i]["email"])), FILTER_VALIDATE_EMAIL)) echo "vale";
+if(filter_var(trim(preg_replace('/[^\x20-\x7E]/', '', $envios_email[0]["email"])), FILTER_VALIDATE_EMAIL)) echo "vale";
 else echo "no vale";
 exit();
 
