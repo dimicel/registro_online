b65ea7b75a88b0c573a07978d91ca2525669ef99
dimicel <39248083+dimicel@users.noreply.github.com>
2025-04-10 13:00:44 +0200

    .

--- php/secret_exencion_fct_email_jd.php
+++ php/secret_exencion_fct_email_jd.php
@@ -2,6 +2,7 @@
 session_start();
 if (!isset($_SESSION['acceso_logueado']) || $_SESSION['acceso_logueado']!=="correcto") exit("Acceso denegado");
 include("mail.php");
+include("conexion.php");
 header("Content-Type: text/html;charset=utf-8");
 
 if ($mysqli->errno>0) exit("server");
