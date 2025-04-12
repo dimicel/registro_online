c8eabddd495b90720abbef5af01c3e4c57574c67
dimicel <39248083+dimicel@users.noreply.github.com>
2025-04-12 23:23:13 +0200

    .

--- js/departamento.js
+++ js/departamento.js
@@ -204,7 +204,7 @@ function verPanelProcesamiento(reg,dirReg){
             contenido +="<div class='col-3'>";
             contenido +="<input type='button' class='textoboton btn btn-success' value='Adjuntar Documento' onclick='adjuntaDocAdicionalExencFCT(\""+resp.registro.id_nie+"\",\""+reg+"\")'/>";
             contenido += "</div></div>";
-            contenido += "<div class='row mt-3' id='div_motivo'><div class='col'>"
+            contenido += "<div class='row mt-3 alertas' id='div_motivo'><div class='col'>"
             contenido += "<span id='rotulo_motivo' class='verReg_label'>MOTIVO NO EXENCIÓN O EXENCIÓN PARCIAL (1000/1000): </span>";
             contenido += "<textarea id='motivo' style='width:100%' onchange='javascript:actualizar=true;' class='verReg_campo form-control' oninput='limiteCaracteres(this)'></textarea>";
             contenido += "</div></div><hr>";
