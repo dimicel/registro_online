b848866e008e2755a578e04c4816110510017194
dimicel <39248083+dimicel@users.noreply.github.com>
2025-04-14 00:24:45 +0200

    .

--- impresos/exencion_fct/exencion_fct.php
+++ impresos/exencion_fct/exencion_fct.php
@@ -202,16 +202,17 @@
     <div id="array_input_type_file" style="display:none"></div>
     <div id="cargando" style="z-index:9999; display:none; position: absolute; left: 0; top: 0; width: 100%; height: 100%; background: white url( 'recursos/espera.gif') no-repeat center center; opacity: .7; "></div>
     <div id="mensaje_div" style="display:none "></div>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <script src=<?php echo "../../jqueryui/jquery-ui.min.js?q=".time();?>></script>
+    <script src=<?php echo "../../js/jquery_validate/jquery.validate.min.js?q=".time();?>></script>
+    <script src=<?php echo "../../js/jquery_validate/additional-methods.min.js?q=".time();?>></script>
+    <script src=<?php echo "../../js/comun.js?q=".time();?> type="text/javascript"></script>
+    <script src=<?php echo "js/exenc.js?q=".time();?> type="text/javascript"></script>
+    <script src=<?php echo "js/validadores_exenfct.js?q=".time();?> type="text/javascript"></script>
+    <script src=<?php echo "../../js/firma.js?q=".time();?> type="text/javascript"></script>
 
 </body>
-<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
-<script src=<?php echo "../../jqueryui/jquery-ui.min.js?q=".time();?>></script>
-<script src=<?php echo "../../js/jquery_validate/jquery.validate.min.js?q=".time();?>></script>
-<script src=<?php echo "../../js/jquery_validate/additional-methods.min.js?q=".time();?>></script>
-<script src=<?php echo "../../js/comun.js?q=".time();?> type="text/javascript"></script>
-<script src=<?php echo "js/exenc.js?q=".time();?> type="text/javascript"></script>
-<script src=<?php echo "js/validadores_exenfct.js?q=".time();?> type="text/javascript"></script>
-<script src=<?php echo "../../js/firma.js?q=".time();?> type="text/javascript"></script>
+
 </html>
