f5d6f304073e9fc11e84087d57ceccb39dbedef4
dimicel <39248083+dimicel@users.noreply.github.com>
2025-04-22 23:01:01 +0200

    .

--- usuario.php
+++ usuario.php
@@ -78,8 +78,8 @@
                 </div>
                 <div class="row justify-content-center">
                     <div class="col-auto p-0" style="padding:0px;">
-                        <div id="div_solicitudes" style="overflow-y:auto; height: 400px; padding: 0;box-sizing: border-box;display:flex; justify-content: center;">
-                            <table id="solicitudes" class="table table-bordered" ></table>
+                        <div id="div_solicitudes" style="overflow-y:auto; height: 400px; padding: 0;box-sizing: border-box;">
+                            <table id="solicitudes"  ></table>
                         </div>
                     </div>
                 </div>
