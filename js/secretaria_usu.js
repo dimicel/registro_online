521cb0070accb254b9e3723813f01d4b5ef136e3
dimicel <39248083+dimicel@users.noreply.github.com>
2025-04-22 20:37:46 +0200

    .

--- js/secretaria_usu.js
+++ js/secretaria_usu.js
@@ -287,13 +287,13 @@ function obtieneDocsExpediente() {
                             }
                             else{
                                 if (resp["docs"][td][j]["resolucion"]!=""){
-                                    contenido_div += "<a target='_blank' style='margin-left:5px' href='"+resp['docs'][td][j]['resolucion']+"'>>Res.Centro<</a></td>";
+                                    contenido_div += "<a target='_blank' style='margin-left:5px' href='"+resp['docs'][td][j]['resolucion']+"?q="+Date.now()+"'>>Res.Centro<</a></td>";
                                 }
                                 if (resp["docs"][td][j]["resolucion_con"]!=""){
-                                    contenido_div += "<a target='_blank' style='margin-left:5px' href='"+resp['docs'][td][j]['resolucion_con']+"'>>Res.Consej.<</a></td>";
+                                    contenido_div += "<a target='_blank' style='margin-left:5px' href='"+resp['docs'][td][j]['resolucion_con']+"?q="+Date.now()+"'>>Res.Consej.<</a></td>";
                                 }
                                 if (resp["docs"][td][j]["resolucion_min"]!=""){
-                                    contenido_div += "<a target='_blank' style='margin-left:5px' href='"+resp['docs'][td][j]['resolucion_min']+"'>>Res.Minist.<</a></td>";
+                                    contenido_div += "<a target='_blank' style='margin-left:5px' href='"+resp['docs'][td][j]['resolucion_min']+"?q="+Date.now()+"'>>Res.Minist.<</a></td>";
                                 }
                             }
                         }
