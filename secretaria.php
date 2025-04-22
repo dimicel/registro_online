f40b655850dad6304bbe510ae632016689f9b237
dimicel <39248083+dimicel@users.noreply.github.com>
2025-04-22 14:37:26 +0200

    .

--- secretaria.php
+++ secretaria.php
@@ -45,13 +45,13 @@
                         <a class="nav-link dropdown-toggle" id="menu1" href="#" data-toggle="dropdown" >
                             Gestión Usuarios
                         </a>
-                        <div class="dropdown-menu">
+                        <div class="dropdown-menu w-100">
                             <a class="nav-link small" href="#" onclick="javascript: panelNuevoUsuario();">Nuevos Usuarios/Cambio de contraseña</a>
                             <a class="nav-link small" href="#" onclick="javascript: verListaUsuarios();">Usuarios - Listado General</a>
                         </div>
                     </li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" id="menu2" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <a class="nav-link dropdown-toggle" id="menu2" href="#" data-toggle="dropdown" >
                             Seleccionar
                         </a>
                         <div class="dropdown-menu" >
