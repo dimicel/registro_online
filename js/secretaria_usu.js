9a6acb0e4c82310d7e6e6b29f61578158f778f0e
dimicel <39248083+dimicel@users.noreply.github.com>
2025-04-22 20:01:24 +0200

    .

--- js/secretaria_usu.js
+++ js/secretaria_usu.js
@@ -508,6 +508,7 @@ function panelExpedienteUsuario(id_nie,nom) {
                 show: { effect: "fade", duration: 0 },
                 title: "EXPEDIENTE DEL USUARIO",
                 width: 800,
+                height:auto,
                 position: { my: "center top", at: "center top", of: window },
                 buttons: [{
                     class: "btn btn-success textoboton",
