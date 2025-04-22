d34ce5a987139560738f01194246a1bc62776643
dimicel <39248083+dimicel@users.noreply.github.com>
2025-04-22 18:42:54 +0200

    .

--- js/secretaria.js
+++ js/secretaria.js
@@ -750,7 +750,9 @@ function listaRegistros(orden_campo, orden_direccion) {
         else {
             document.getElementById("div_notabla").style.display = "none";
             document.getElementById("div_tabla").style.display = "block";
-            document.getElementById("div_tabla").style.width=document.getElementById("encabezado_docs").style.width;
+            if (document.getElementById("registros_docs").scrollHeight > document.getElementById("registros_docs").clientHeight) {
+                document.getElementById("div_tabla").style.width=document.getElementById("encabezado_docs").style.width+20+"px";
+            }
             //encab = "";
             data = "";
             data_array = resp["registros"];
