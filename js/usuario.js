10675331c5e7f2adac90b5b3ee29d400e3806d36
dimicel <39248083+dimicel@users.noreply.github.com>
2025-04-22 22:44:49 +0200

    .

--- js/usuario.js
+++ js/usuario.js
@@ -519,7 +519,6 @@ function listaSolicitudes() {
             listado.innerHTML = tabla;
             if (document.getElementById("div_solicitudes").scrollHeight > document.getElementById("div_solicitudes").clientHeight) {
                 document.getElementById("div_solicitudes").style.width=listado.offsetWidth+"px";
-                alert("a"+document.getElementById("div_solicitudes").style.width+"    b"+document.getElementById("div_solicitudes").offsetWidth);
                 //document.getElementById("div_solicitudes").style.marginLeft="20px";
             }
             
