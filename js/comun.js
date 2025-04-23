abaefaaa0423e6c06e7f55cef073c28eba028bf5
dimicel <39248083+dimicel@users.noreply.github.com>
2025-04-23 14:33:34 +0200

    .

--- js/comun.js
+++ js/comun.js
@@ -96,6 +96,7 @@ function confirmarAccion(mensaje, titulo, ancho) {
             ],
             open: function() {
                 // Aplicamos clases Bootstrap al vuelo
+                alert()
                 let $buttons = $div.parent().find(".ui-dialog-buttonpane button");
                 $buttons.eq(0).addClass("textoboton btn btn-success btn-sm");
                 $buttons.eq(1).addClass("textoboton btn btn-danger btn-sm");
