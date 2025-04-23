c092a3f67d46b5d7a59a562f2105fda4cb7b3d76
dimicel <39248083+dimicel@users.noreply.github.com>
2025-04-23 19:04:58 +0200

    .

--- js/departamento.js
+++ js/departamento.js
@@ -225,7 +225,7 @@ function verPanelProcesamiento(reg,dirReg){
             document.getElementById("verRegistro_div").innerHTML = contenido;
             if (resp.registro.resolucion!="PENDIENTE" && resp.registro.resolucion!="") document.getElementById("valoracion_informe").value=resp.registro.resolucion;
             document.getElementById("motivo").value=resp.registro.motivo;
-            seleccionValoracion(resp.registro.valoracion);
+            seleccionValoracion(resp.registro.resolucion);
             if (resp.registro.procesado==1){
                 document.getElementById("btn_generar_informe").disabled=true;
                 document.getElementById("div_firma").style.display="none";
