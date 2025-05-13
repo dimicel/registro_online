3567d905c7049057ff5fb376a71800af45afa494
dimicel <39248083+dimicel@users.noreply.github.com>
2025-05-13 12:50:25 +0200

    .

--- secretaria.php
+++ secretaria.php
@@ -591,7 +591,7 @@
                     <span class="errorTxt" style="font-size: 1em;"></span>
                     <input type="text" id="dpto_abreviatura" name="dpto_abreviatura" class="form-control" maxlength="4" readonly>
                 </div>
-                <div class="col-5 " style="text-align: center;display:none" id="div_boton_guardar_cambios">
+                <div class="col-5 d-flex flex-column justify-content-end" style="text-align: center;visibility:hidden" id="div_boton_guardar_cambios">
                     <div class="w-100 d-flex justify-content-end">
                         <button type="button" id="btn_nuevo_dpto" class="btn btn-success btn-sm" onclick="guardaCambiosDpto()">Guardar Cambios</button>
                         <button type="button" id="btn_cancela_nuevo_dpto" class="btn btn-danger btn-sm ml-2" onclick="gestionSeleccionDpto();this.parent.style.display='none'">Cancelar</button>
