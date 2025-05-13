3a6303f4347d38ae35d9a51979d9f2c8d14815f4
dimicel <39248083+dimicel@users.noreply.github.com>
2025-05-13 12:35:07 +0200

    .

--- js/secretaria.js
+++ js/secretaria.js
@@ -3030,6 +3030,8 @@ function eliminaPrematriculas(){
 
 
 function gestionDptos(){
+    document.getElementById("config_dpto").selectedIndex=0;
+    gestionSeleccionDpto();
     $("#div_departamentos").dialog({
         autoOpen: true,
         dialogClass: "no-close",
