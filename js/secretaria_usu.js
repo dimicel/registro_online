9c0b2c757536359793447d848a82ec3fd2ab700d
dimicel <39248083+dimicel@users.noreply.github.com>
2025-05-14 21:25:58 +0200

    .

--- js/secretaria_usu.js
+++ js/secretaria_usu.js
@@ -338,7 +338,8 @@ function obtieneDocsExpediente() {
 
 
 function borraDocExp(obj) {
-    $("#div_dialogs2").load("html/secretaria.htm?q="+Date.now()+" #div_borra_doc", function(response,status, xhr){
+    //$("#div_dialogs2").load("html/secretaria.htm?q="+Date.now()+" #div_borra_doc", function(response,status, xhr){
+    $("body").append("html/secretaria.htm?q="+Date.now()+" #div_borra_doc", function(response,status, xhr){
         if ( status == "error" ) {
             var msg = "Error en la carga de procedimiento: " + xhr.status + " " + xhr.statusText;
             alerta(msg,"ERROR DE CARGA");
@@ -372,7 +373,7 @@ function borraDocExp(obj) {
                 maxHeight: 500,
                 width: 550,
                 close:function(event,ui){
-                    $("#div_dialogs2").dialog("destroy");
+                    $("#div_dialogs2").dialog("destroy").remove();
                 }
             });
         }
