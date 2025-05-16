576d005ab7aae9b95fca3b86578d88fa49141400
dimicel <39248083+dimicel@users.noreply.github.com>
2025-05-16 11:53:15 +0200

    .

--- js/secretaria_usu.js
+++ js/secretaria_usu.js
@@ -383,7 +383,6 @@ function obtieneDocsExpediente() {
 function borraDocExp(obj) {
     cargaHTML("html/secretaria.htm", "div_borra_doc","BORRADO DE DOCUMENTO DEL EXPEDIENTE",550,500)
     .then ((dialogo)=>{
-            alert(dialogo.innerHTML);
             _del_ruta_completa = obj.parentElement.children[1].children[0].href;
             _del_ruta = ".." + _del_ruta_completa.substr(_del_ruta_completa.indexOf("/docs/"));
             _del_curso = obj.parentElement.children[0].innerHTML;
