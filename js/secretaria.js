233f2bc395258d31b4fdb29dcde3690e6f0751f9
dimicel <39248083+dimicel@users.noreply.github.com>
2025-05-19 11:33:21 +0200

    .

--- js/secretaria.js
+++ js/secretaria.js
@@ -1678,32 +1678,6 @@ function panelNuevoUsuario() {
     });
 }
 
-function generaPass() {
-    mayus = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-    minus = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-    nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-    matriz = [];
-    password = "";
-    matriz.length = 8;
-    matriz[0] = mayus[Math.trunc(Math.random() * (mayus.length - 1))];
-    matriz[1] = minus[Math.trunc(Math.random() * (minus.length - 1))];
-    matriz[2] = nums[Math.trunc(Math.random() * (nums.length - 1))];
-    matriz[3] = mayus[Math.trunc(Math.random() * (mayus.length - 1))];
-    matriz[4] = minus[Math.trunc(Math.random() * (minus.length - 1))];
-    matriz[5] = nums[Math.trunc(Math.random() * (nums.length - 1))];
-    matriz[6] = mayus[Math.trunc(Math.random() * (mayus.length - 1))];
-    matriz[7] = minus[Math.trunc(Math.random() * (minus.length - 1))];
-    //matriz=matriz.sort(function(){Math.random()-0.5});
-    //Desordena el array
-    var j, x, i;
-    for (i = matriz.length - 1; i > 0; i--) {
-        j = Math.floor(Math.random() * (i + 1));
-        x = matriz[i];
-        matriz[i] = matriz[j];
-        matriz[j] = x;
-    }
-    return matriz.join('');
-}
 
 
 function altaUsuario() {
