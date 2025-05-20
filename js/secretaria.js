0e2f58abae47762c404189419369aa955f72214f
dimicel <39248083+dimicel@users.noreply.github.com>
2025-05-20 17:28:37 +0200

    .

--- js/secretaria.js
+++ js/secretaria.js
@@ -3031,7 +3031,7 @@ function gestionDptos(){
                 }
             }
         ]
-    ).then((dialogo)=>{alert(dialog.innerHTML)
+    ).then((dialogo)=>{alert(dialogo.innerHTML);
         document.getElementById("cargando").style.display = "none";
         generaSelectsDepartamentos();
         document.getElementById("dpto_select").selectedIndex=0;
