dce6ea78cf5e3b9d91b9835feec23b86f8aadd86
dimicel <39248083+dimicel@users.noreply.github.com>
2025-05-22 09:40:16 +0200

    .

--- usuario.php
+++ usuario.php
@@ -343,19 +343,7 @@
     <!--_______________________________________________________________________________________________-->
     <!--_______________________________________________________________________________________________-->
 
-    <div id="div_edita_imagen_usu" style="display:none; text-align:center">
-        <label><small id="texto_editor_imagen">Rota, haz zoom (con la rueda del ratón) y mueve la imagen para ajustarla al recuadro</small></label>
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                    <div class="row">
-                        <div class="col-12 justify-content-center align-items-center" id="div_imagen">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div> 
-    </div>
+
 
 
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
