c6a8682c35daa20c03ab08016e7d30824203d56b
dimicel <39248083+dimicel@users.noreply.github.com>
2025-05-23 08:55:51 +0200

    .

--- impresos/matriculas/mat_eso.php
+++ impresos/matriculas/mat_eso.php
@@ -92,20 +92,6 @@
     </div>
 
 
-    <div id="div_edita_imagen" style="display:none; text-align:center">
-        <label><small id="texto_editor_imagen">Rota, haz zoom (con la rueda del ratón) y mueve la imagen para ajustarla al recuadro</small></label>
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                    <div class="row">
-                        <div class="col-12 justify-content-center align-items-center" id="div_imagen">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div> 
-    </div>
-
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
