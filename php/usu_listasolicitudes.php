1b46210f9f2720dee25a1e1b1505a690bfdedd5f
dimicel <39248083+dimicel@users.noreply.github.com>
2025-05-30 12:44:44 +0200

    .

--- php/usu_listasolicitudes.php
+++ php/usu_listasolicitudes.php
@@ -232,7 +232,7 @@ if ($consulta->num_rows>0){
     }
 }
 $consulta->free();
-exit();
+exit(json_encode($data));
 $consulta=$mysqli->query("select * where id_nie='$id_nie' order by curso,fecha_registro, registro");
 if ($consulta->num_rows>0){
     $data["error"]="ok";
@@ -243,7 +243,7 @@ if ($consulta->num_rows>0){
     }
 }
 $consulta->free();
-//exit();
+exit(json_encode($data));
 $contador=0;
 $consulta=$mysqli->query("select * from mat_ciclos where id_nie='$id_nie' order by curso,fecha_registro, registro");
 if ($consulta->num_rows>0){
