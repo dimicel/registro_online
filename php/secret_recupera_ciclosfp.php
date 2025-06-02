3f5245c70376ade2fa6454bd759f4c56fe76edc0
dimicel <39248083+dimicel@users.noreply.github.com>
2025-06-02 13:20:29 +0200

    .

--- php/secret_recupera_ciclosfp.php
+++ php/secret_recupera_ciclosfp.php
@@ -62,7 +62,7 @@ if ($grado!=="") {
 }
 
 $consulta="select * from ciclos where " . $filtro . " order by descripcion ASC, grado ASC";
-$data["consulta"]=$consulta;    
+$data["consulta"]=$diurno."    ".$vespertino."    ".$nocturno."    ".$elearning."    ".$dpto."    ".$grado;    
 exit(json_encode($data));
 
 $res=$mysqli->query($consulta);
