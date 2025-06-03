cf702153ce6b1b67c10a96274706444ab3342615
dimicel <39248083+dimicel@users.noreply.github.com>
2025-06-03 09:42:24 +0200

    .

--- js/secretaria.js
+++ js/secretaria.js
@@ -611,6 +611,7 @@ function generaTablaCiclosFP(pantallaEspera=true) {
                 const celdaCursos = document.createElement("td");
                 celdaCursos.innerHTML = resp.registro[i].cursos;
                 celdaCursos.style.width = ancho_cursos;
+                celdaCursos.style.textAlign = 'center';
                 celdaCursos.style.boxSizing = "border-box";
                 celdaCursos.style.overflow = "hidden";
                 celdaCursos.style.textOverflow = "ellipsis";
