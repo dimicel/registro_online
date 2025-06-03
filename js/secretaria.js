8d1aa5e2585e48b120ecb45c3c8638a89086693f
dimicel <39248083+dimicel@users.noreply.github.com>
2025-06-03 16:30:54 +0200

    .

--- js/secretaria.js
+++ js/secretaria.js
@@ -3890,10 +3890,7 @@ function guardaAnadeCicloFP(obj){
         for (var i=0;i<campos.length;i++){
             if( document.getElementById("ciclo_"+campos[i]).type=="checkbox"){
                 if (document.getElementById("ciclo_"+campos[i]).checked!=document.getElementById("backup_"+campos[i]).value==="true"){
-                    alert(document.getElementById("ciclo_"+campos[i]).checked);
-                    alert(document.getElementById("backup_"+campos[i]).value);
                     modificado=true;
-                    alert(0);
                     break;
                 }
             }
