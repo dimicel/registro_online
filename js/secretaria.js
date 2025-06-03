0ccaf61958a5a7f4508162e95e399323a5cf8438
dimicel <39248083+dimicel@users.noreply.github.com>
2025-06-03 03:08:09 +0200

    .

--- js/secretaria.js
+++ js/secretaria.js
@@ -542,7 +542,7 @@ function generaTablaCiclosFP(pantallaEspera=true) {
             thead.style.color = "white";
             thead.style.zIndex = "2";
             thead.style.display = "table";
-            thead.style.width = "99%";
+            thead.style.width = "98.5%";
             thead.style.tableLayout = "fixed";
 
             // Estilo de cada th
