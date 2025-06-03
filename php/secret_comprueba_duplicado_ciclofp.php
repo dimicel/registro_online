a1ba4a2444b19e41adfd5c967d665a686531e0a7
dimicel <39248083+dimicel@users.noreply.github.com>
2025-06-03 20:14:05 +0200

    .

---
+++ php/secret_comprueba_duplicado_ciclofp.php
@@ -0,0 +1,59 @@
+<?php
+session_start();
+if (!isset($_SESSION['acceso_logueado']) || $_SESSION['acceso_logueado']!=="correcto") exit("Acceso denegado");
+include("conexion.php");
+include("funcion_normalizacion_valor.php");
+header("Content-Type: text/html;charset=utf-8");
+
+$data=array();
+if ($mysqli->errno>0) {
+    exit("server");
+}
+
+
+$ciclo=$_POST['ciclo'];
+$accion=$_POST['accion'];
+$id=$_POST['id'];
+$ciclo_normalizado = normalizar_nombre($ciclo);
+
+$mysqli->set_charset("utf8");
+
+
+if ($accion=="alta") {
+    $sql="SELECT * FROM ciclos WHERE descripcion='$ciclo'";
+} elseif ($accion=="modifica") {
+    $sql="SELECT * FROM modulosfp WHERE descripcion='$ciclo' AND id!='$id'";
+}
+
+$resultado=$mysqli->query($sql);
+if ($mysqli->errno>0) {
+    exit("server");
+}
+if ($resultado->num_rows>0) {
+    exit("duplicado");
+}
+
+if ($accion=="alta") {
+    $sql="SELECT * FROM ciclos";
+} elseif ($accion=="modifica") {
+    $sql="SELECT * FROM ciclos WHERE id!='$id'";
+}
+$coincidencia="";
+$resultado=$mysqli->query($sql);   
+if ($mysqli->errno>0) {
+    exit("server");
+}
+if ($resultado->num_rows>0) {
+    while ($row = $resultado->fetch_assoc()) {
+        if (normalizar_nombre($row['descripcion']) == $ciclo_normalizado) {
+            $coincidencia = "duplicado_normalizado";
+            break;
+        }
+    }
+}
+
+if($coincidencia=="duplicado_normalizado") {
+    exit("duplicado_normalizado");
+} else {
+    exit("ok");
+}   
