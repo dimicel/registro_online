c0a793643bae44528b36b32a0f56311ed78ed2bf
dimicel <39248083+dimicel@users.noreply.github.com>
2025-06-07 20:31:55 +0200

    .

--- php/secret_recupera_modulos_cursosfp.php
+++ php/secret_recupera_modulos_cursosfp.php
@@ -2,7 +2,8 @@
 session_start();
 if (!isset($_SESSION['acceso_logueado']) || $_SESSION['acceso_logueado']!=="correcto") exit("Acceso denegado");
 include("conexion.php");
-header("Content-Type: text/html;charset=utf-8");
+header("Content-Type: application/json;charset=utf-8");
+
 
 $data=array();
 if ($mysqli->errno>0) {
