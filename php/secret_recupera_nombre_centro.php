14ed51670032dd4ae886bf26d74ad09158675c3c
dimicel <39248083+dimicel@users.noreply.github.com>
2025-06-09 10:56:03 +0200

    .

---
+++ php/secret_recupera_nombre_centro.php
@@ -0,0 +1,22 @@
+<?php
+include("conexion.php");
+header("Content-Type: text/html;charset=utf-8");
+
+$data=array();
+if ($mysqli->errno>0) {
+    $data["error"]="server";
+    exit(json_encode($data));
+}
+
+$res=$mysqli->query("select * from config_centro");
+
+$data["error"]="ok";
+$data["registro"]=array();
+
+while ($reg=$res->fetch_assoc()){
+    $data["registro"]["centro"]= $reg["centro"];
+}
+
+
+exit(json_encode($data));
+
