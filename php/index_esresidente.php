d59d8f5cbc5e4240087e54bf72e68beff65c2f28
dimicel <39248083+dimicel@users.noreply.github.com>
2025-06-11 10:21:37 +0200

    .

---
+++ php/index_esresidente.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+include("conexion.php");
+header("Content-Type: text/html;charset=utf-8");
+header("Expires: Tue, 01 Jul 2001 06:00:00 GMT");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");	
+
+if ($mysqli->errno>0) {
+    exit("server");
+}
+
+$id_nie=$_POST["usuario"];
+
+$consulta=$mysqli->query("select * from usuarios where id_nie='$id_nie'");
+if ($consulta->num_rows>0){
+    $consulta->free();
+    exit("si");
+} 
+else{
+    $consulta->free();
+    exit("no");
+}
+
+
