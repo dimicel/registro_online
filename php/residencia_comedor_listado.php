edd85d83a310b833ad5339bf60f58bb568460bca
dimicel <39248083+dimicel@users.noreply.github.com>
2025-06-13 13:05:47 +0200

    .

--- php/residencia_comedor_listado.php
+++ php/residencia_comedor_listado.php
@@ -17,9 +17,13 @@ $fecha_mysql = $fecha->format('Y-m-d');
 
 $lista_avisos="select * from residentes_comedor where fecha_no_comedor='$fecha_mysql'";
 $con_avisos=$mysqli->query($lista_avisos);
+$list_avisos=array();
 if ($con_avisos->num_rows>0){
-    
+    while($d=$con_avisos->fetch_assoc(MYSQLI_ASSOC)){
+        $list_avisos[]=$d["id_nie"];
+    }
 }
+$con_avisos->free();
 
 $consulta="SELECT * FROM residentes  where curso='$curso' and baja=0 ";
 $res=$mysqli->query($consulta);
@@ -33,6 +37,8 @@ $contador=0;
 $data["registros"]=array();
 
 while ($reg=$res->fetch_assoc()){
+    if (in_array($reg["id_nie"],$list_avisos)) $data["registros"][$contador]["avisado"]=1;
+    else $data["registros"][$contador]["avisado"]=0;
     $data["registros"][$contador]["id_nie"]= $reg["id_nie"];
     $data["registros"][$contador]["nombre"]=ucwords(strtolower($reg["apellidos"])).", ".ucwords(strtolower($reg["nombre"]));
     $contador++;
