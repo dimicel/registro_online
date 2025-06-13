de83d7173ceae4af99c2dfb763b12d6751d8d825
dimicel <39248083+dimicel@users.noreply.github.com>
2025-06-13 11:57:41 +0200

    .

--- residencia.php
+++ residencia.php
@@ -82,7 +82,7 @@
                     </select>
                 </div>
                 <div class="col-2">
-                    <input type="button" class="textoboton btn btn-success" onclick="residenciaGestionComedor()" value="comedor">
+                    <input type="button" class="textoboton btn btn-success" onclick="residenciaGestionComedor()" value="Comedor">
                 </div>
             </div>
             <div class="row">
