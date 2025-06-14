d359cdc40c1c3481948008d88d4fc9cc1141cca1
dimicel <39248083+dimicel@users.noreply.github.com>
2025-06-14 23:47:38 +0200

    .

--- js/comedor.js
+++ js/comedor.js
@@ -6,7 +6,7 @@ $(function() {
         if (resp["error"] != "ok") document.write(resp["error"]);
         else{
             ocultarPantallaEspera();
-            curso=resp.anno_ini_curso+"-"+resp.anno_ini_curso+1;
+            curso=resp.anno_ini_curso+"-"+parseInt(resp.anno_ini_curso)+1;
             $("#fecha_lista_comedor").datepicker({
                 changeMonth: true,
                 changeYear: true,
