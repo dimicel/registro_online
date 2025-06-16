f9179a3ae9f4f46e24e0cf019fcb3d19899b6ddb
dimicel <39248083+dimicel@users.noreply.github.com>
2025-06-16 14:01:53 +0200

    .

--- php/residencia_csv_ausencias_com.php
+++ php/residencia_csv_ausencias_com.php
@@ -7,7 +7,7 @@ $Datos="";
 include("conexion.php");
 if ($mysqli->errno>0) $error="Error en servidor.";
 
-$curso=$_POST["comedor_curso"];
+$curso=$_POST["comedor_curso"];exit();
 $mes=$_POST["mes_informe"];
 $anno_1=$substr($curso, 0, 4);
 $anno_2=$substr($curso, -4);
@@ -15,7 +15,7 @@ $array_meses=array("Ene","Feb","Mar","Abr","May","Jun","Jul","Ago","Sep","Oct","
 $array_dias_mes=array(31,29,31,30,31,30,31,31,30,31,30,31);
 $mes_anno="";
 $fecha_inicio="";
-$fecha_fin="";exit();
+$fecha_fin="";
 if ((int)$mes>=7 && (int)$mes<=12) {
     $mes_anno=$array_meses[(int)$mes-1]."/".$anno_1;
     $fecha_inicio = $anno_1."-".str_pad($mes, 2, "0", STR_PAD_LEFT)."-01";
