861a3d502a456ef7c4cad0149f97dd9a5210d93c
dimicel <39248083+dimicel@users.noreply.github.com>
2025-06-16 18:56:59 +0200

    .

--- php/residencia_csv_ausencias_com.php
+++ php/residencia_csv_ausencias_com.php
@@ -63,7 +63,7 @@ if ($mes_num >= 7 && $mes_num <= 12) {
 
 $Name = 'informe_no_asistencia_comedor_' . $mes_anno . '.csv';
 
-$Datos .= "INFORME FALTAS DE ASISTENCIA AL COMEDOR NO COMUNICADAS $mes_anno" . PHP_EOL;
+$Datos .= "INFORME FALTAS DE ASISTENCIA AL COMEDOR NO COMUNICADAS - " . strtoupper($mes_anno) . PHP_EOL;
 $Datos .= 'NIE;APELLIDOS;NOMBRE;CURSO_ACTUAL;FECHA' . PHP_EOL;
 
 // Consulta SQL
