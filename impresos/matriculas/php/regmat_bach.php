88e76abc2515286cfc48ca6f49487c5b2c727d90
dimicel <39248083+dimicel@users.noreply.github.com>
2025-06-21 21:38:34 +0200

    .

--- impresos/matriculas/php/regmat_bach.php
+++ impresos/matriculas/php/regmat_bach.php
@@ -149,31 +149,6 @@ else{
 }
 //GENERA EL PDF Y LO GUARDA EN EL SERVIDOR
 
-/*class MYPDF extends TCPDF {
-
-	//Page header
-	public function Header() {
-		// Logo
-		$image_file = '../../../recursos/logo_ccm.jpg';
-		$this->Image($image_file, 10, 10, 25, '', 'JPG', '', 'T', false, 300, '', false, false, 0, false, false, false);
-		$image_file = '../../../recursos/mini_escudo.jpg';
-		$this->Image($image_file, 140, 10, 20, '', 'JPG', '', 'T', false, 300, '', false, false, 0, false, false, false);
-
-		$this->SetFont('helvetica', 'B', 14);
-		$this->SetXY(0,10);
-		$this->Cell(0,0,"M A T R Í C U L A",0,0,'C',0,'',1,false,'T','T');
-			
-		$this->SetFont('helvetica', '', 8);
-		// Title
-		//$this->setCellHeightRatio(1.75);
-		$encab = "<label><strong>IES Universidad Laboral</strong><br>Avda. Europa, 28<br>45003-TOLEDO<br>Tlf.:925 22 34 00<br>Fax:925 22 24 54</label>";
-		$this->writeHTMLCell(0, 0, 160, 11, $encab, 0, 1, 0, true, 'C', true);
-		//$this->Ln();
-		//$this->writeHTMLCell(0, 0, '', '', '', 'B', 1, 0, true, 'L', true);
-		
-	}
-}*/
-
 // create new PDF document
 $titulo_PDF = "M A T R Í C U L A";
 $pdf = new MYPDF($datos_cen, $titulo_PDF);
