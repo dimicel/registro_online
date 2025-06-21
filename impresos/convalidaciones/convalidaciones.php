5fecc2fb3293d2b5175dbddbf0780734d157073c
dimicel <39248083+dimicel@users.noreply.github.com>
2025-06-21 23:07:07 +0200

    .

--- impresos/convalidaciones/convalidaciones.php
+++ impresos/convalidaciones/convalidaciones.php
@@ -22,7 +22,7 @@
             <!--CABECERA  ------------------------------------------------------------------------------------>
             <div class="d-flex flex-row justify-content-center" style="margin-top:30px">
                 <div class="col-2 justify-content-start">
-                    <input style="width:175px; height:100px" type="image" src="recursos/logo_ccm.jpg" />
+                    <input style="width:175px; height:100px" type="image" src="../../../recursos/logo_ccm.jpg" />
                 </div>
                 <div class="col-8 justify-content-center" style="text-align: center;">
                     <h5>IES UNIVERSIDAD LABORAL DE TOLEDO</h5>
@@ -31,7 +31,7 @@
                     <h5 id="rotulo_curso" style="color:#900; font-weight:bold">CURSO ACTUAL:</h5>
                 </div>
                 <div class="col-2 justify-content-end">
-                    <input style="width:150px; height:150px" type="image" src="recursos/mini_escudo.jpg" />
+                    <input style="width:150px; height:150px" type="image" src="../../../recursos/mini_escudo.jpg" />
                 </div>
             </div>
         </div>
