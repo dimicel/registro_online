8ba01051ffdb34c3f36204d7a095a3b231819dc5
dimicel <39248083+dimicel@users.noreply.github.com>
2025-06-21 22:33:22 +0200

    .

--- php/cabecera_pdf.php
+++ php/cabecera_pdf.php
@@ -18,9 +18,9 @@ class MYPDF extends TCPDF {
 	//Page header
 	public function Header() {
 		// Logo
-		$image_file = '../recursos/logo_ccm.jpg';
+		$image_file = __DIR__ . '/../recursos/logo_ccm.jpg';
 		$this->Image($image_file, 10, 10, 25, '', 'JPG', '', 'T', false, 300, '', false, false, 0, false, false, false);
-		$image_file = '../recursos/mini_escudo.jpg';
+		$image_file = __DIR__ . '/../recursos/mini_escudo.jpg';
 		$this->Image($image_file, 140, 10, 20, '', 'JPG', '', 'T', false, 300, '', false, false, 0, false, false, false);
 
 		$this->SetFont('helvetica', 'B', 12);
