2011fb838c1161800c12efd3b926e17fcbb80914
dimicel <39248083+dimicel@users.noreply.github.com>
2025-06-21 21:15:06 +0200

    .

--- impresos/convalidaciones/php/registraformulario.php
+++ impresos/convalidaciones/php/registraformulario.php
@@ -89,7 +89,7 @@ while($repite_registro){
     $vReg->free();
 }
 $dirRegistro=substr($registro, 17);
-exit;
+exit ("ok");
 ///Parametro de bind sss por la siguiente tabla
 //"i": Entero (integer)
 //"d": Decimal (double)
