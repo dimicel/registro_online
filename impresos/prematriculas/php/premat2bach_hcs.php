7537db75ced17fdc74e6580307cbdbadead0d386
dimicel <39248083+dimicel@users.noreply.github.com>
2025-06-22 10:39:49 +0200

    .

--- impresos/prematriculas/php/premat2bach_hcs.php
+++ impresos/prematriculas/php/premat2bach_hcs.php
@@ -6,28 +6,12 @@ include("../../../php/conexion.php");
 if ($mysqli->errno>0) {
     exit("servidor");
 }
-
+include("../../../php/funciones.php");
 // Cargamos las librerías de TCPDF
 require_once(__DIR__.'/../../../php/tcpdf/config/tcpdf_config_alt.php');
 require_once(__DIR__.'/../../../php/tcpdf/tcpdf.php');
 include("../../../php/cabecera_pdf.php");
 
-function generaRegistro(){
-    $minus="abcdefghijklmnopqrstuvwxyz";
-    $nums="0123456789";
-    $array=array("","","","","","","","");
-    $registro="";
-    $array[0]=substr($nums,mt_rand(0,strlen("mayus")-1),1);
-    $array[1]=substr($minus,mt_rand(0,strlen("minus")-1),1);
-    $array[2]=substr($nums,mt_rand(0,strlen("nums")-1),1);
-    $array[3]=substr($minus,mt_rand(0,strlen("mayus")-1),1);
-    $array[4]=substr($nums,mt_rand(0,strlen("minus")-1),1);
-    $array[5]=substr($minus,mt_rand(0,strlen("nums")-1),1);
-    $array[6]=substr($nums,mt_rand(0,strlen("mayus")-1),1);
-    $array[7]=substr($minus,mt_rand(0,strlen("signos")-1),1);
-    shuffle($array);
-    return "iesulabto_pm2bah_".date('dmY')."_".$array[0].$array[1].$array[2].$array[3].$array[4].$array[5].$array[6].$array[7];;    
-}
 
 
 $curso=$_POST['curso'];
@@ -78,13 +62,13 @@ $espitin17=$_POST['b2h_eitin27'];
 
 
 
-$registro=generaRegistro();
+$registro=generaRegistro("iesulabto_pm2bah_");
 $repite_registro=true;
 while ($repite_registro){
     $res=$mysqli->query("select * from premat_bach where registro='$registro'");
     if ($mysqli->errno>0) exit("servidor");
     if ($res->num_rows>0){
-       $registro= generaRegistro(); 
+       $registro= generaRegistro("iesulabto_pm2bah_"); 
     }
     else if ($res->num_rows==0){
         $repite_registro=false;
