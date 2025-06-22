c7318ec6362604b7c8d8a81ccdfab16080dcb76b
dimicel <39248083+dimicel@users.noreply.github.com>
2025-06-22 10:53:40 +0200

    .

--- impresos/revision_examen/php/generapdf.php
+++ impresos/revision_examen/php/generapdf.php
@@ -11,45 +11,10 @@ include("../../../php/mail.php");
 if ($mysqli->errno>0) {
     exit("servidor");
 }
-
+include("../../../php/funciones.php");
 require_once(__DIR__.'/../../../php/tcpdf/config/tcpdf_config_alt.php');
 require_once(__DIR__.'/../../../php/tcpdf/tcpdf.php');
 include("../../../php/cabecera_pdf.php");
-
-
-function generaRegistro(){
-    $minus="abcdefghijklmnopqrstuvwxyz";
-    $nums="0123456789";
-    $array=array("","","","","","","","");
-    $registro="";
-    $array[0]=substr($nums,mt_rand(0,strlen("mayus")-1),1);
-    $array[1]=substr($minus,mt_rand(0,strlen("minus")-1),1);
-    $array[2]=substr($nums,mt_rand(0,strlen("nums")-1),1);
-    $array[3]=substr($minus,mt_rand(0,strlen("mayus")-1),1);
-    $array[4]=substr($nums,mt_rand(0,strlen("minus")-1),1);
-    $array[5]=substr($minus,mt_rand(0,strlen("nums")-1),1);
-    $array[6]=substr($nums,mt_rand(0,strlen("mayus")-1),1);
-    $array[7]=substr($minus,mt_rand(0,strlen("signos")-1),1);
-    shuffle($array);
-    return "iesulabto_revexa_".date('dmY')."_".$array[0].$array[1].$array[2].$array[3].$array[4].$array[5].$array[6].$array[7];;    
-}
-
-function quitaAcentos($s){	
-    $s=str_replace("á","a",$s);
-    $s=str_replace("é","e",$s);
-    $s=str_replace("í","i",$s);
-    $s=str_replace("ó","o",$s);
-    $s=str_replace("ú","u",$s);
-    $s=str_replace("Á","A",$s);
-    $s=str_replace("É","E",$s);
-    $s=str_replace("Í","I",$s);
-    $s=str_replace("Ó","O",$s);
-    $s=str_replace("Ú","U",$s);
-    $s=str_replace("1ª","Primera",$s);
-    $s=str_replace("2ª","Segunda",$s);
-    return $s;
-}
-
 
 function calculaCurso(){
     $mes=(int)date("n");
@@ -94,13 +59,13 @@ elseif ($pass_nif=="PASS"){
  else if ($padre=="PADRE") $padres="PADRE/MADRE";
  else if ($padre=="TUTOR") $padres="TUTOR/A";
  
- $registro= generaRegistro();
+ $registro= generaRegistro("iesulabto_revexa_");
 
 $repite_registro=true;
 while ($repite_registro){
     $res=$mysqli->query("select * from revision_examen where registro='$registro'");
     if ($res->num_rows>0){
-       $registro= generaRegistro(); 
+       $registro= generaRegistro("iesulabto_revexa_"); 
     }
     else if ($res->num_rows==0){
         $repite_registro=false;
