ef2d7f3cd5cc0e5be3bd23ded7635c8553162e62
dimicel <39248083+dimicel@users.noreply.github.com>
2025-06-22 09:43:35 +0200

    .

--- php/index_esresidente.php
+++ php/index_esresidente.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 include("conexion.php");
+include("funciones.php");
 header("Content-Type: text/html;charset=utf-8");
 header("Expires: Tue, 01 Jul 2001 06:00:00 GMT");
 header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
@@ -41,11 +42,4 @@ else{
     exit(json_encode($respuesta));
 }
 
-function calculaCurso_ini(){
-    $mes=(int)date("n");
-    $anno=(int)date("Y");
-    if ($mes>=7 && $mes<=12) 
-        return $anno;
-    else
-        return $anno-1;
-}
+
