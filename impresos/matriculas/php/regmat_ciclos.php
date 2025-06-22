ff58b5749249572ac78e4007c3a39bf728b8200e
dimicel <39248083+dimicel@users.noreply.github.com>
2025-06-22 10:32:23 +0200

    .

--- impresos/matriculas/php/regmat_ciclos.php
+++ impresos/matriculas/php/regmat_ciclos.php
@@ -6,28 +6,11 @@ include("../../../php/conexion.php");
 if ($mysqli->errno>0) {
     exit("servidor");
 }
-
+include("../../../php/funciones.php");
 // Include TCPDF library
 require_once(__DIR__.'/../../../php/tcpdf/config/tcpdf_config_alt.php');
 require_once(__DIR__.'/../../../php/tcpdf/tcpdf.php');
 include("../../../php/cabecera_pdf.php");
-
-function generaRegistro(){
-    $minus="abcdefghijklmnopqrstuvwxyz";
-    $nums="0123456789";
-    $array=array("","","","","","","","");
-    $registro="";
-    $array[0]=substr($nums,mt_rand(0,strlen("mayus")-1),1);
-    $array[1]=substr($minus,mt_rand(0,strlen("minus")-1),1);
-    $array[2]=substr($nums,mt_rand(0,strlen("nums")-1),1);
-    $array[3]=substr($minus,mt_rand(0,strlen("mayus")-1),1);
-    $array[4]=substr($nums,mt_rand(0,strlen("minus")-1),1);
-    $array[5]=substr($minus,mt_rand(0,strlen("nums")-1),1);
-    $array[6]=substr($nums,mt_rand(0,strlen("mayus")-1),1);
-    $array[7]=substr($minus,mt_rand(0,strlen("signos")-1),1);
-    shuffle($array);
-    return "iesulabto_macicl_".date('dmY')."_".$array[0].$array[1].$array[2].$array[3].$array[4].$array[5].$array[6].$array[7];;    
-}
 
 $grado=$_POST['sel_grado'];
 $ciclo=$_POST['sel_ciclos'];
@@ -77,13 +60,13 @@ $mysqli->query("delete from mat_bach where id_nie='$id_nie' and curso='$anno_cur
 $mysqli->query("delete from mat_fpb where id_nie='$id_nie' and curso='$anno_curso'");
 $mysqli->query("delete from mat_ciclos where id_nie='$id_nie' and curso='$anno_curso'");
 
-$registro=generaRegistro();
+$registro=generaRegistro("iesulabto_macicl_");
 $repite_registro=true;
 while ($repite_registro){
     $res=$mysqli->query("select registro from mat_ciclos where registro='$registro'");
     if ($mysqli->errno>0) exit("servidor");
     if ($res->num_rows>0){
-        $registro= generaRegistro($desc_reg[$curso]); 
+        $registro= generaRegistro("iesulabto_macicl_"); 
     }
     else if ($res->num_rows==0){
         $repite_registro=false;
--- impresos/matriculas/php/regmat_fpb.php
+++ impresos/matriculas/php/regmat_fpb.php
@@ -6,29 +6,11 @@ include("../../../php/conexion.php");
 if ($mysqli->errno>0) {
     exit("servidor");
 }
-
+include("../../../php/funciones.php");
 // Requiere TCPDF y la cabecera del PDF
 require_once(__DIR__.'/../../../php/tcpdf/config/tcpdf_config_alt.php');
 require_once(__DIR__.'/../../../php/tcpdf/tcpdf.php');
 include("../../../php/cabecera_pdf.php");
-
-function generaRegistro(){
-    $minus="abcdefghijklmnopqrstuvwxyz";
-    $nums="0123456789";
-    $array=array("","","","","","","","");
-    $registro="";
-    $array[0]=substr($nums,mt_rand(0,strlen("mayus")-1),1);
-    $array[1]=substr($minus,mt_rand(0,strlen("minus")-1),1);
-    $array[2]=substr($nums,mt_rand(0,strlen("nums")-1),1);
-    $array[3]=substr($minus,mt_rand(0,strlen("mayus")-1),1);
-    $array[4]=substr($nums,mt_rand(0,strlen("minus")-1),1);
-    $array[5]=substr($minus,mt_rand(0,strlen("nums")-1),1);
-    $array[6]=substr($nums,mt_rand(0,strlen("mayus")-1),1);
-    $array[7]=substr($minus,mt_rand(0,strlen("signos")-1),1);
-    shuffle($array);
-    return "iesulabto_matfpb_".date('dmY')."_".$array[0].$array[1].$array[2].$array[3].$array[4].$array[5].$array[6].$array[7];;    
-}
-
 
 $ciclo=$_POST['sel_ciclos'];
 $curso_ciclo=$_POST['sel_curso'];
@@ -61,13 +43,13 @@ $provincia=$_POST['provincia'];
 $tutor=$_POST['tutor'];
 $fecha_registro=date('Y-m-d');
 
-$registro=generaRegistro();
+$registro=generaRegistro("iesulabto_matfpb_");
 $repite_registro=true;
 while ($repite_registro){
     $res=$mysqli->query("select * from mat_fpb where registro='$registro'");
     if ($mysqli->errno>0) exit("servidor");
     if ($res->num_rows>0){
-       $registro= generaRegistro(); 
+       $registro= generaRegistro("iesulabto_matfpb_"); 
     }
     else if ($res->num_rows==0){
         $repite_registro=false;
