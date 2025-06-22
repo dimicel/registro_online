ef40366e4f8bb897f86addd41da9678107f9ffa8
dimicel <39248083+dimicel@users.noreply.github.com>
2025-06-22 10:37:19 +0200

    .

--- impresos/prematriculas/php/premat3eso.php
+++ impresos/prematriculas/php/premat3eso.php
@@ -6,29 +6,11 @@ include("../../../php/conexion.php");
 if ($mysqli->errno>0) {
     exit("servidor");
 }
-
+include("../../../php/funciones.php");
 // Include TCPDF library
 require_once(__DIR__.'/../../../php/tcpdf/config/tcpdf_config_alt.php');
 require_once(__DIR__.'/../../../php/tcpdf/tcpdf.php');
 include("../../../php/cabecera_pdf.php");
-
-function generaRegistro(){
-    $minus="abcdefghijklmnopqrstuvwxyz";
-    $nums="0123456789";
-    $array=array("","","","","","","","");
-    $registro="";
-    $array[0]=substr($nums,mt_rand(0,strlen("mayus")-1),1);
-    $array[1]=substr($minus,mt_rand(0,strlen("minus")-1),1);
-    $array[2]=substr($nums,mt_rand(0,strlen("nums")-1),1);
-    $array[3]=substr($minus,mt_rand(0,strlen("mayus")-1),1);
-    $array[4]=substr($nums,mt_rand(0,strlen("minus")-1),1);
-    $array[5]=substr($minus,mt_rand(0,strlen("nums")-1),1);
-    $array[6]=substr($nums,mt_rand(0,strlen("mayus")-1),1);
-    $array[7]=substr($minus,mt_rand(0,strlen("signos")-1),1);
-    shuffle($array);
-    return "iesulabto_pm3eso_".date('dmY')."_".$array[0].$array[1].$array[2].$array[3].$array[4].$array[5].$array[6].$array[7];;    
-}
-
 
 $curso=$_POST['curso'];
 $anno_curso=$_POST['anno_curso'];
@@ -67,13 +49,13 @@ $opt2=$_POST['eso3_opt2'];
 $opt3=$_POST['eso3_opt3'];
 $opt4=$_POST['eso3_opt4'];
 
-$registro=generaRegistro();
+$registro=generaRegistro("iesulabto_pm3eso_");
 $repite_registro=true;
 while ($repite_registro){
     $res=$mysqli->query("select * from premat_eso where registro='$registro'");
     if ($mysqli->errno>0) exit("servidor");
     if ($res->num_rows>0){
-       $registro= generaRegistro(); 
+       $registro= generaRegistro("iesulabto_pm3eso_"); 
     }
     else if ($res->num_rows==0){
         $repite_registro=false;
--- impresos/prematriculas/php/premat3esodiv.php
+++ impresos/prematriculas/php/premat3esodiv.php
@@ -6,28 +6,11 @@ include("../../../php/conexion.php");
 if ($mysqli->errno>0) {
     exit("servidor");
 }
-
+include("../../../php/funciones.php");
 // Cargamos las librerías de TCPDF
 require_once(__DIR__.'/../../../php/tcpdf/config/tcpdf_config_alt.php');
 require_once(__DIR__.'/../../../php/tcpdf/tcpdf.php');
 include("../../../php/cabecera_pdf.php");
-
-function generaRegistro(){
-    $minus="abcdefghijklmnopqrstuvwxyz";
-    $nums="0123456789";
-    $array=array("","","","","","","","");
-    $registro="";
-    $array[0]=substr($nums,mt_rand(0,strlen("mayus")-1),1);
-    $array[1]=substr($minus,mt_rand(0,strlen("minus")-1),1);
-    $array[2]=substr($nums,mt_rand(0,strlen("nums")-1),1);
-    $array[3]=substr($minus,mt_rand(0,strlen("mayus")-1),1);
-    $array[4]=substr($nums,mt_rand(0,strlen("minus")-1),1);
-    $array[5]=substr($minus,mt_rand(0,strlen("nums")-1),1);
-    $array[6]=substr($nums,mt_rand(0,strlen("mayus")-1),1);
-    $array[7]=substr($minus,mt_rand(0,strlen("signos")-1),1);
-    shuffle($array);
-    return "iesulabto_pm3esd_".date('dmY')."_".$array[0].$array[1].$array[2].$array[3].$array[4].$array[5].$array[6].$array[7];;    
-}
 
 
 $curso=$_POST['curso'];
@@ -60,13 +43,13 @@ $opt2=$_POST['eso3_div_opt2'];
 $opt3=$_POST['eso3_div_opt3'];
 $opt4=$_POST['eso3_div_opt4'];
 
-$registro=generaRegistro();
+$registro=generaRegistro("iesulabto_pm3esd_");
 $repite_registro=true;
 while ($repite_registro){
     $res=$mysqli->query("select * from premat_eso where registro='$registro'");
     if ($mysqli->errno>0) exit("servidor");
     if ($res->num_rows>0){
-       $registro= generaRegistro(); 
+       $registro= generaRegistro("iesulabto_pm3esd_"); 
     }
     else if ($res->num_rows==0){
         $repite_registro=false;
