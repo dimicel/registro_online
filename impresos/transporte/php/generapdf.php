3af1b7ae18fa8cae33403c2d69bad499985f8ac0
dimicel <39248083+dimicel@users.noreply.github.com>
2025-06-22 10:56:00 +0200

    .

--- impresos/transporte/php/generapdf.php
+++ impresos/transporte/php/generapdf.php
@@ -6,28 +6,10 @@ include("../../../php/conexion.php");
 if ($mysqli->errno>0) {
     exit("servidor");
 }
-
-$mysqli->set_charset("utf8");
+include("../../../php/funciones.php");
 require_once(__DIR__.'/../../../php/tcpdf/config/tcpdf_config_alt.php');
 require_once(__DIR__.'/../../../php/tcpdf/tcpdf.php');
 include("../../../php/cabecera_pdf.php");
-
-function generaRegistro($proc){
-    $minus="abcdefghijklmnopqrstuvwxyz";
-    $nums="0123456789";
-    $array=array("","","","","","","","");
-    $registro="";
-    $array[0]=substr($nums,mt_rand(0,strlen("mayus")-1),1);
-    $array[1]=substr($minus,mt_rand(0,strlen("minus")-1),1);
-    $array[2]=substr($nums,mt_rand(0,strlen("nums")-1),1);
-    $array[3]=substr($minus,mt_rand(0,strlen("mayus")-1),1);
-    $array[4]=substr($nums,mt_rand(0,strlen("minus")-1),1);
-    $array[5]=substr($minus,mt_rand(0,strlen("nums")-1),1);
-    $array[6]=substr($nums,mt_rand(0,strlen("mayus")-1),1);
-    $array[7]=substr($minus,mt_rand(0,strlen("signos")-1),1);
-    shuffle($array);
-    return "iesulabto_".$proc."_".date('dmY')."_".$array[0].$array[1].$array[2].$array[3].$array[4].$array[5].$array[6].$array[7];;    
-}
 
 if (!isset($_POST["id_nie"])) {
     exit ("Acceso denegado");
@@ -70,13 +52,13 @@ $fecha_registro=date('Y-m-d');
 
 $mysqli->query("delete from transporte where id_nie='$id_nie' and curso='$anno_curso'");
 
-$registro=generaRegistro("transp");
+$registro=generaRegistro("iesulabto_transp_");
 $repite_registro=true;
 while ($repite_registro){
     $res=$mysqli->query("select registro from transporte where registro='$registro'");
     if ($mysqli->errno>0) exit("servidor");
     if ($res->num_rows>0){
-        $registro= generaRegistro("transp"); 
+        $registro= generaRegistro("iesulabto_transp_"); 
     }
     else if ($res->num_rows==0){
         $repite_registro=false;
