842bb28d0b5fa1059603985a7fb1e96ff7c6a2e2
dimicel <39248083+dimicel@users.noreply.github.com>
2025-07-02 15:32:28 +0200

    .

--- impresos/matriculas/js/mat_bach.js
+++ impresos/matriculas/js/mat_bach.js
@@ -61,6 +61,7 @@ $(document).ready(function() {
         if (res2["error"] == "ok") {
             existe_premat = true;
             curso_prematricula = res2["curso_prematricula"];
+            _curso= curso_prematricula;
             (typeof(res2["sexo"]) !== "undefined" || res2["sexo"]===null) ? sexo = res2["sexo"]: sexo = "";
             (typeof(res2["tutor1"]) !== "undefined" || res2["tutor1"]===null) ? tutor1 = res2["tutor1"]: tutor1 = "";
             (typeof(res2["tutor2"]) !== "undefined" || res2["tutor2"]===null) ? tutor2 = res2["tutor2"]: tutor2 = "";
