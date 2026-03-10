995f9c076ddd8a482e040eb250517ce41a7d3e67
dimicel <dimicel@gmail.com>
2026-03-10 14:08:32 +0100

    .

--- impresos/transporte/js/trans.js
+++ impresos/transporte/js/trans.js
@@ -202,7 +202,7 @@ function registraSolicitud() {
             window.history.back();
             window.history.back();
         } 
-        else window.history.back();
+        //else window.history.back();
     });
 }
 
