1487e2c11faa89e7df3cc3704f0ab3068ed11db7
dimicel <dimicel@gmail.com>
2026-03-11 12:02:19 +0100

    .

--- impresos/residencia/php/mail.php
+++ impresos/residencia/php/mail.php
@@ -1,4 +1,8 @@
-<?php
+<?
+/*ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+*/
 
 require __DIR__.'/../../../PHPmailer/src/Exception.php';
 require __DIR__.'/../../../PHPmailer/src/PHPMailer.php';
