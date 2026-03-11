247f71a4136e99ee39d0c7a1a124b242aae3db46
dimicel <dimicel@gmail.com>
2026-03-11 12:17:25 +0100

    .

--- impresos/exencion_fct/php/ciclos.php
+++ impresos/exencion_fct/php/ciclos.php
@@ -1,6 +1,10 @@
 <?php
 
 session_start();
+/*ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+*/
 if (!isset($_SESSION['acceso_logueado']) || $_SESSION['acceso_logueado']!=="correcto") exit("Acceso denegado");
 include("../../../php/conexion.php");
 
