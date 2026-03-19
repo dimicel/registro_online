8366e42660400a5a8ae487c9ffb6bc27ddb742be
dimicel <dimicel@gmail.com>
2026-03-19 20:42:36 +0100

    .

--- php/usu_moddatospers.php
+++ php/usu_moddatospers.php
@@ -84,7 +84,9 @@ try {
     if (!$stmt2->execute()) throw new Exception("Error en 'usuarios_dat': " . $stmt2->error);
 
     // D. BLOQUE CONDICIONAL (Segundo script): Solo si NO es 'alumno'
-    if ($usuario !== 'alumno') {
+    //De momento se ionhabilita que el cambio de datos se propague a todas las tablas asociadas a formularios
+    //if ($usuario !== 'alumno') {
+    if ($usuario == 'no se hace nunca') {
         $tablas = $mysqli->query("SHOW TABLES");
         if ($tablas) {
             while ($fila = $tablas->fetch_row()) {
