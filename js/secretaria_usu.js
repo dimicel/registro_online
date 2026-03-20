bc9346eb2cccc1dfc7471e76ee6318205bc42b8c
dimicel <dimicel@gmail.com>
2026-03-20 11:51:11 +0100

    .

--- js/secretaria_usu.js
+++ js/secretaria_usu.js
@@ -698,6 +698,17 @@ function panelModUsu(id) {
                 return validaDNI_NIE(value, esPasaporte);
             }, "Identificación no válida");
 
+            jQuery.validator.addMethod("nif_noduplicado", function(value, element) {
+                if (value.miTrim() == '') return true;
+                $.ajaxSetup({ async: false });
+                var a = $.post("php/usu_nifduplicado.php", { nu_nif: value, id_nie: id_nie }, function(resp) {
+                    if (resp == "ok") _nif_duplicado = true;
+                    else _nif_duplicado = false;
+                });
+                $.ajaxSetup({ async: true });
+                return _nif_duplicado;
+            });
+
             $("#form_modif_datos_usu").validate({
                 rules: {
                     mod_nombre: {
