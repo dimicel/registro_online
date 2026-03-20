f06efb371509edc50c77c425fd8e31880e976176
dimicel <dimicel@gmail.com>
2026-03-20 12:18:17 +0100

    .

--- js/secretaria.js
+++ js/secretaria.js
@@ -1975,6 +1975,7 @@ function altaUsuario() {
         if(NifNoDuplicado(document.getElementById("nr_nif").value)){
             $('#nr_nif').removeClass('is-invalid').addClass('is-valid');
         }
+        alert(NifNoDuplicado(document.getElementById("nr_nif").value))
         if(!validaDNI_NIE(document.getElementById("nr_nif").value) || !NifNoDuplicado(document.getElementById("nr_nif").value)){
             if (!validaDNI_NIE(document.getElementById("nr_nif").value)){
                 document.getElementById("nr_nif").setCustomValidity("El NIF o NIE no es válido");
