004b42c274cc6971456a4d709c34c3e102682ef3
dimicel <dimicel@gmail.com>
2026-04-02 17:17:22 +0200

    .

--- php/residencia_csv_ausencias_com.php
+++ php/residencia_csv_ausencias_com.php
@@ -158,11 +158,11 @@ if ($formato=="excel"){
     $output = fopen('php://output', 'w');
     fprintf($output, chr(0xEF).chr(0xBB).chr(0xBF));
     if ($error!="") {
+        fputcsv($output, $formato, ";");
         fputcsv($output, [$error], ';');
         fclose($output);
         exit();
     }
-    fputcsv($output, $formato, ";");
     fputcsv($output, $primera_fila, ";");
     fputcsv($output, $encabezamiento, ";");
 
