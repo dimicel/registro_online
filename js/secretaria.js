44417e615c9e1e90b58da0e668ef991f0cf2bfc9
dimicel <dimicel@gmail.com>
2026-04-10 09:35:15 +0200

    .

--- js/secretaria.js
+++ js/secretaria.js
@@ -4627,7 +4627,7 @@ function obtieneRutasParadas(ruta=""){
                                         confirmar('Se eliminará la parada ${resp.paradas[i].parada}.','CONFIMAR BORRADO')
                                         .then(function(confirmacion){
                                             if (confirmacion){
-                                            alert("eliminar");
+                                            alert('eliminar');
                                                 //eliminarRuta(${resp.rutas[i].ruta}); 
                                             }                 
                                         });">
