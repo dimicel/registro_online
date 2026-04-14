8279a823068ce0c37a55c50bbb365fdaf6f1f768
dimicel <dimicel@gmail.com>
2026-04-14 12:32:34 +0200

    .

--- js/secretaria.js
+++ js/secretaria.js
@@ -4469,7 +4469,6 @@ function RutasTransporte(){
 
 
 function obtieneRutasParadas(ruta=""){
-    alert(ruta);
         mostrarPantallaEspera();
         $.post("php/secret_transporte_recupera_rutas.php",{ruta:ruta},(resp)=>{
             ocultarPantallaEspera();
