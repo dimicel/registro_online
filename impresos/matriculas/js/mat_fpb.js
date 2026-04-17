1fb94df6b614f9d383c955fd511f1d6be10a0fa5
dimicel <dimicel@gmail.com>
2026-04-17 11:33:15 +0200

    .

--- impresos/matriculas/js/mat_fpb.js
+++ impresos/matriculas/js/mat_fpb.js
@@ -15,7 +15,7 @@ var _paginas = new Array();
 var paginas_totales;
 var sexo="",fecha_nac="",telef_alumno="",email_alumno="",direccion="",cp="",localidad="",provincia="";
 var tutor1="",email_tutor1="",tlf_tutor1="",tutor2="",email_tutor2="",tlf_tutor2="";
-var existe_foto=false, existe_dni_A=false, existe_dni_R=false, existe_seguro=false,existe_certificado=false;
+var existe_foto=false, existe_dni_A=false, existe_dni_R=false, existe_seguro=false,existe_certificado=false,existe_num_ss=false;
 var primera_vez_pag_2=true;
 var primera_vez_pag_3=true;
 
@@ -85,6 +85,8 @@ $(document).ready(function() {
         else existe_seguro=false;
         if (resp.indexOf('C')>-1) existe_certificado=true;
         else existe_certificado=false;
+        if (resp.indexOf('N')>-1) existe_num_ss=true;
+        else existe_num_ss=false;
 
         $("#pagina_1").load("fpb_html/pagina1.html?q="+Date.now().toString(), function() {
             creaValidatorPagina1();
@@ -280,6 +282,18 @@ function pasaPagina(p) {
                 $(this).text($(this).text().replace('(Opcional) ', '*'));
             });
             
+            // --- LÓGICA NUM SS
+            $("#num_ss").show();
+            if (existe_num_ss) {
+                $("#div_num_ss").hide();
+                $("#div_existe_num_ss").show();
+                $("#num_ss").rules("remove", "required");
+            } else {
+                $("#div_num_ss").show();
+                $("#div_existe_num_ss").hide();
+                $("#num_ss").rules("add", { required: true });
+            }
+
 
             // --- LÓGICA SEGURO ESCOLAR
             $("#sec_seguro").show();
@@ -327,6 +341,9 @@ function cargaImagen(dest){
     }
     else if(dest=="prev_resguardo_seguro"){
         alerta("<center><img src='../../docs/"+id_nie+"/seguro/"+anno_curso+"/"+id_nie+".jpeg?q="+Date()+"'></center>","RESGUARDO SEGURO ESCOLAR",false,700);
+    }
+    else if(dest=="prev_num_ss"){
+        alerta("<center><img src='../../docs/"+id_nie+"/nss/nss_"+id_nie+".jpeg?q="+Date()+"'></center>","NÚMERO SEGURIDAD SOCIAL",false,700);
     }
 }
 
