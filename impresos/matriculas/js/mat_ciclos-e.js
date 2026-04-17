cfead3d6de968220e8be0b86bdf1d294a20b47bb
dimicel <dimicel@gmail.com>
2026-04-17 11:46:57 +0200

    .

--- impresos/matriculas/js/mat_ciclos-e.js
+++ impresos/matriculas/js/mat_ciclos-e.js
@@ -533,10 +533,12 @@ function mayor28() {
 
 
 function muestraAyudaDocs(){
-    var mensaje_docs = "<p>Los documentos y sus formatos son los siguientes:";
+    var mensaje_docs = "<p style='font-size:0.75em;'>";
+    mensaje_docs += "Los documentos y sus formatos son los siguientes:";
     mensaje_docs += "<ul>";
     mensaje_docs += "    <li>Fotografía del alummno: en formato JPEG tomada con móvil en vertical y fondo blanco, como se muestra en la imagen:<br><center><img src='../../recursos/foto_carne.jpg'  style='width:128px;'></center></li>";
     mensaje_docs += "    <li>Fotografía del anverso y reverso del documento de identificación (DNI/NIE). Si sólo tiene pasaporte, el anverso será imagen JPEG de la página en la que salen los datos del alumno y su fotografía, y el reverso imagen JPEG en blanco. El documento se fotografiará con el móvil en horizontal y fondo blanco, por ejemplo, poniendo el documento sobre un folio en blanco.</li>";
+    mensaje_docs += "    <li>Captura de pantalla en formato JPEG del Número de la Seguridad Social facilitado por la Tesorería General de la Seguridad Social en su página web. No es necesario que la captura sea de la página completa, es suficiente con que se vea el número de forma clara.</li>";
     mensaje_docs += "    <li>EXCEPTO nacidos antes del 31/12/" + (anno_ini_curso-27) + ", una fotografía del resguardo del pago del seguro escolar, y del anverso y reverso del documento de identificación (DNI/NIE). (Móvil en horizontal y fondo blanco, por ejemplo, sobre un folio).</li>";
     mensaje_docs += "    <li>Si es alumno nuevo e inició los estudios de los que se matricula en otra comunidad autónoma, certificado de notas en formato PDF (puede escanearlo, por ejemplo, con la aplicación gratuita para móvil Microsoft Office Lens).</li>";
     mensaje_docs += "</ul>";
