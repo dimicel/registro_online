79e637ebe0765fddc0f4a56100a02983c6d6589c
dimicel <dimicel@gmail.com>
2026-04-21 14:05:32 +0200

    .

--- php/usu_obtiene_nivel_matriculado.php
+++ php/usu_obtiene_nivel_matriculado.php
@@ -31,7 +31,8 @@ if ($stmt) {
     $stmt->execute();
     $resultado = $stmt->get_result();
 
-    if ($fila = $resultado->fetch_assoc()) {
+    if ($resultado->num_rows > 0) {
+        $fila = $resultado->fetch_assoc();
         echo $fila['tabla'];
     } else {
         echo "no_matricula";
