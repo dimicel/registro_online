8c07d729f590fe8135d6055e30812425bcf3fd01
dimicel <dimicel@gmail.com>
2026-04-23 09:21:14 +0200

    .

--- secretaria.php
+++ secretaria.php
@@ -200,13 +200,13 @@
                 </form>
                 <form class="form-inline">
                     <div class="form-group row" id="div_transporte_escolar" style="display:none">
-                        <label class="col-form-label col-lg-2" >Ruta: </label>
+                        <label class="col-form-label col-lg-2" for="transporte_ruta">Ruta: </label>
                         <div class="col-lg-5">
-                            <select id="transporte_ruta" size="1" style="margin-left: -60px;" onchange="generaSelectTransporteParada(this.value);listaRegistros();" class="form-control">
+                            <select id="transporte_ruta" size="1" style="margin-left: -20px;" onchange="generaSelectTransporteParada(this.value);listaRegistros();" class="form-control">
                                 
                             </select>
                         </div>
-                        <label class="col-form-label col-lg-1" style="margin-left: 20px;">Parada: </label>
+                        <label class="col-form-label col-lg-1" style="margin-left: 20px;" for="transporte_parada">Parada: </label>
                         <div class="col-lg-1">
                             <select id="transporte_parada"  size="1" onchange="listaRegistros();" class="form-control">
                                 
