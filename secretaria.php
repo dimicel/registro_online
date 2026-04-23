8bfb01f4365110b9221dee99f80c8644e284cda9
dimicel <dimicel@gmail.com>
2026-04-23 10:36:11 +0200

    .

--- secretaria.php
+++ secretaria.php
@@ -199,20 +199,20 @@
                     </div>
                 </form>
                 <form class="form-inline">
-                    <div class="form-group row" id="div_transporte_escolar" style="display:none">
-                            <label class="col-form-label col-lg-2" for="transporte_ruta" style="margin-left: 20px;">Ruta: </label>
-                            <div class="col-lg-4">
-                                <select id="transporte_ruta" size="1" style="margin-left: -20px;" onchange="generaSelectTransporteParada(this.value);" class="form-control">
-                                    
-                                </select>
-                            </div>
-                            <label class="col-form-label col-lg-2"  for="transporte_parada" >Parada: </label>
-                            <div class="col-lg-4" style="margin-left: 20px;">
-                                <select id="transporte_parada"  size="1" onchange="listaRegistros();" class="form-control">
-                                    <option value="" selected>Todas</option>
-                                </select>
-                            </div>
+                    <div class="form-group row" id="div_transporte_escolar" style="display:none; margin-left:20px;">
+                        <label class="col-form-label col-lg-2" for="transporte_ruta" >Ruta: </label>
+                        <div class="col-lg-4">
+                            <select id="transporte_ruta" size="1"  onchange="generaSelectTransporteParada(this.value);" class="form-control">
+                                
+                            </select>
                         </div>
+                        <label class="col-form-label col-lg-2"  for="transporte_parada" >Parada: </label>
+                        <div class="col-lg-4" >
+                            <select id="transporte_parada"  size="1" onchange="listaRegistros();" class="form-control">
+                                <option value="" selected>Todas</option>
+                            </select>
+                        </div>
+                    </div>
                 </form>
                 <form class="form-inline">
                     <div class="form-group row" id="div_curso_mat_ciclos" style="display:none">
