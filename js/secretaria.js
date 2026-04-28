5374bbd763fa2668c9e6c5d9b5d8f16e9192db12
dimicel <dimicel@gmail.com>
2026-04-28 14:14:40 +0200

    .

--- js/secretaria.js
+++ js/secretaria.js
@@ -2357,6 +2357,13 @@ function verDocsMatricula(id, edad) {
                     document.getElementById("img_num_ss").style.display="none";
                     document.getElementById("txt_no_num_ss").style.display="";
                 }
+                if(existe_seguro){
+                    document.getElementById("img_seguro").src=`docs/${id}/seguro/${_curso}/${id}.jpeg?q="+Date()`;
+                }
+                else {
+                    document.getElementById("img_seguro").style.display="none";
+                    document.getElementById("txt_no_seguro").style.display="";
+                }
             });
 
         }
