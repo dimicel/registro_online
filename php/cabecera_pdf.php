94193db51578e21c9ec62e3560d5f84625cf2f16
dimicel <dimicel@gmail.com>
2026-05-04 12:39:59 +0200

    .

--- php/cabecera_pdf.php
+++ php/cabecera_pdf.php
@@ -28,7 +28,8 @@ class MYPDF extends TCPDF {
 
 		$this->SetFont('helvetica', 'B', 12);
 		$this->SetXY(0,0);
-		$this->writeHTMLCell(105, 0, 35, 10, '<div style="text-align:center; font-weight:bold;">' . $this->titulo . '</div>', 0, 1, 0, true, 'C', true);
+		$this->writeHTMLCell(105, 0, 35, 5, '<h3 style="color:red;">FORMULARIO NO VÁLIDO SIN LA FORMALIZACIÓN DE MATRÍCULA EN EDUCAMOS</h3>', 0, 1, 0, true, 'C', true);
+		$this->writeHTMLCell(105, 0, 35, 15, '<div style="text-align:center; font-weight:bold;">' . $this->titulo . '</div>', 0, 1, 0, true, 'C', true);
 			
 		$this->SetFont('helvetica', '', 8);
 
