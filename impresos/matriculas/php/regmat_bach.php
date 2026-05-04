ead9653e4fbf468d40476dc2c7c4ae9a27afe8d7
dimicel <dimicel@gmail.com>
2026-05-04 13:24:00 +0200

    .

--- impresos/matriculas/php/regmat_bach.php
+++ impresos/matriculas/php/regmat_bach.php
@@ -15,11 +15,6 @@ include("../../../php/funciones.php");
 require_once(__DIR__.'/../../../php/tcpdf/tcpdf.php');
 include("../../../php/cabecera_pdf.php");
 
-if (!isset($datos_cen)) {
-    exit("Error crítico: La variable $datos_cen no se ha definido en cabecera_pdf.php");
-}
-else exit("siiiiiii");
-
 $desc_reg = Array(
     "1º Bach. HH.CC.SS."=>"ma1bah",
     "1º Bach. Ciencias y Tecnología"=>"ma1bac",
