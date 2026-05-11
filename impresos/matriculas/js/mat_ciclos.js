725ea446b9702f6a8ef658a9f88413b4377f9883
dimicel <dimicel@gmail.com>
2026-05-11 10:28:08 +0200

    .

--- impresos/matriculas/js/mat_ciclos.js
+++ impresos/matriculas/js/mat_ciclos.js
@@ -506,6 +506,7 @@ function CreaSelCurso(c) {
 function CreaSelTurno(t) {
     mostrarPantallaEspera();
     $.post("php/obtiene_curso_anterior.php",{curso:document.getElementById("anno_curso").value},(resp)=>{
+        alert(0);
         ocultarPantallaEspera();
         if (resp.error=="ok"){
             ciclo_anterior=resp.datos.ciclo;
